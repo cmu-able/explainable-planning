915aba26432b9ad41fed0cb91dea1562548291f4
Roykrong Sukkerd <rsukkerd@andrew.cmu.edu>
2018-03-06 18:45:01 -0500

    Added getTravelTime method To TravelTimeQFunction to make function structure explicit

--- XPlanning/src/examples/mobilerobot/metrics/TravelTimeQFunction.java
+++ XPlanning/src/examples/mobilerobot/metrics/TravelTimeQFunction.java
@@ -1,12 +1,13 @@
 package examples.mobilerobot.metrics;
 
 import examples.mobilerobot.factors.Distance;
+import examples.mobilerobot.factors.LocationStateVar;
 import examples.mobilerobot.factors.MoveToAction;
 import examples.mobilerobot.factors.RobotSpeed;
+import examples.mobilerobot.factors.SpeedStateVar;
 import exceptions.AttributeNameNotFoundException;
 import exceptions.QValueNotFound;
 import exceptions.VarNameNotFoundException;
-import factors.IAction;
 import factors.Transition;
 import metrics.IQFunction;
 
@@ -18,17 +19,26 @@ import metrics.IQFunction;
  */
 public class TravelTimeQFunction implements IQFunction {
 
+	public double getTravelTime(LocationStateVar rLocSrc, SpeedStateVar rSpeedSrc, MoveToAction moveTo,
+			LocationStateVar rLocDest) throws AttributeNameNotFoundException {
+		Distance distance = moveTo.getDistance(rLocSrc);
+		RobotSpeed speed = rSpeedSrc.getSpeed();
+		return distance.getDistance() / speed.getSpeed();
+	}
+
 	@Override
-	public double getValue(Transition trans) 
+	public double getValue(Transition trans)
 			throws VarNameNotFoundException, QValueNotFound, AttributeNameNotFoundException {
-		IAction action = trans.getAction();
-		if (action instanceof MoveToAction) {
-			Distance distance = (Distance) action.getDerivedAttributeValue("distance", trans.getSrcStateVar("rLoc"));
-			RobotSpeed rSpeedSrc = (RobotSpeed) trans.getSrcStateVar("rSpeed");
-			return distance.getDistance() / rSpeedSrc.getSpeed();
-		} else {
-			return 0;
+		if (trans.getSrcStateVar("rLoc") instanceof LocationStateVar
+				&& trans.getSrcStateVar("rSpeed") instanceof SpeedStateVar && trans.getAction() instanceof MoveToAction
+				&& trans.getDestStateVar("rLoc") instanceof LocationStateVar) {
+			LocationStateVar rLocSrc = (LocationStateVar) trans.getSrcStateVar("rLoc");
+			SpeedStateVar rSpeedSrc = (SpeedStateVar) trans.getSrcStateVar("rSpeed");
+			MoveToAction moveTo = (MoveToAction) trans.getAction();
+			LocationStateVar rLocDest = (LocationStateVar) trans.getSrcStateVar("rLoc");
+			return getTravelTime(rLocSrc, rSpeedSrc, moveTo, rLocDest);
 		}
+		return 0;
 	}
 
 }

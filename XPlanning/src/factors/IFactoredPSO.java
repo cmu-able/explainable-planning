0b25e16d253285bbf3ac3a540b687cd164d7f59b
Roykrong Sukkerd <rsukkerd@andrew.cmu.edu>
2018-03-09 18:14:04 -0500

    Added factored probabilistic STRIPS operator for action representation

---
+++ XPlanning/src/factors/IFactoredPSO.java
@@ -0,0 +1,11 @@
+package factors;
+
+/**
+ * {@link IFactoredPSO} is an interface to a "factored" probabilistic STRIPS operator representation.
+ * 
+ * @author rsukkerd
+ *
+ */
+public interface IFactoredPSO {
+
+}

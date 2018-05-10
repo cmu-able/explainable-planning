64255be59cf64232b2e9657c19f8c4e579c08c94
Roykrong Sukkerd <rsukkerd@andrew.cmu.edu>
2018-05-10 19:45:21 -0400

    PrismConnector: compute and cache QA values of a generated policy

--- XPlanning/src/prismconnector/PrismConnector.java
+++ XPlanning/src/prismconnector/PrismConnector.java
@@ -3,7 +3,10 @@ package prismconnector;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
 
+import dtmc.XDTMC;
 import exceptions.ActionDefinitionNotFoundException;
 import exceptions.ActionNotFoundException;
 import exceptions.AttributeNameNotFoundException;
@@ -17,15 +20,19 @@ import exceptions.ResultParsingException;
 import exceptions.VarNotFoundException;
 import mdp.State;
 import mdp.XMDP;
+import metrics.IQFunction;
 import policy.Policy;
 import prism.PrismException;
 
 public class PrismConnector {
 
+	private XMDP mXMDP;
 	private PrismAPIWrapper mPrismAPI;
 	private Map<Policy, Double> mCachedTotalCosts = new HashMap<>();
+	private Map<Policy, Map<IQFunction, Double>> mCachedQAValues = new HashMap<>();
 
-	public PrismConnector() throws PrismException {
+	public PrismConnector(XMDP xmdp) throws PrismException {
+		mXMDP = xmdp;
 		mPrismAPI = new PrismAPIWrapper();
 	}
 
@@ -55,11 +62,52 @@ public class PrismConnector {
 				mdpTranslator.getValueEncodingScheme(), outputPath);
 		Map<Integer, State> stateIndices = explicitModelReader.readStatesFromFile(staOutputFilename);
 		Policy policy = explicitModelReader.readPolicyFromFile(traOutputFilename, stateIndices);
+
+		// Cache the expected total cost of the policy
 		mCachedTotalCosts.put(policy, totalCost);
+
+		// Compute and cache the QA values of the policy
+		computeQAValues(policy, mXMDP.getQFunctions());
+
 		return policy;
 	}
 
 	public double getExpectedTotalCost(Policy policy) {
 		return mCachedTotalCosts.get(policy);
+	}
+
+	public double getQAValue(Policy policy, IQFunction qFunction) {
+		return mCachedQAValues.get(policy).get(qFunction);
+	}
+
+	private void computeQAValues(Policy policy, Set<IQFunction> qFunctions)
+			throws ActionDefinitionNotFoundException, EffectClassNotFoundException, VarNotFoundException,
+			IncompatibleVarException, ActionNotFoundException, DiscriminantNotFoundException,
+			IncompatibleActionException, IncompatibleEffectClassException, IncompatibleDiscriminantClassException,
+			AttributeNameNotFoundException, PrismException, ResultParsingException {
+		XDTMC xdtmc = new XDTMC(mXMDP, policy);
+		PrismDTMCTranslator dtmcTranslator = new PrismDTMCTranslator(xdtmc, true);
+		String dtmcWithQAs = dtmcTranslator.getDTMCTranslationWithQAs();
+
+		Map<IQFunction, String> queryProperties = new HashMap<>();
+		StringBuilder builder = new StringBuilder();
+		for (IQFunction qFunction : qFunctions) {
+			String queryProperty = dtmcTranslator.getNumQueryPropertyTranslation(qFunction);
+			queryProperties.put(qFunction, queryProperty);
+			builder.append(queryProperty);
+			builder.append("\n");
+		}
+		String propertiesStr = builder.toString();
+
+		Map<String, Double> results = mPrismAPI.queryPropertiesFromDTMC(dtmcWithQAs, propertiesStr);
+
+		// Cache the QA values of the policy
+		Map<IQFunction, Double> qaValues = new HashMap<>();
+		for (Entry<IQFunction, String> entry : queryProperties.entrySet()) {
+			IQFunction qFunction = entry.getKey();
+			String queryProperty = entry.getValue();
+			qaValues.put(qFunction, results.get(queryProperty));
+		}
+		mCachedQAValues.put(policy, qaValues);
 	}
 }

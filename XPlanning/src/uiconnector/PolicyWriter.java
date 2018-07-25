38c033d6a82f291711c133ce4ce39ef5fda5a584
Roykrong Sukkerd <rsukkerd@andrew.cmu.edu>
2018-07-25 15:03:39 -0400

    writePolicy()

--- XPlanning/src/uiconnector/PolicyWriter.java
+++ XPlanning/src/uiconnector/PolicyWriter.java
@@ -7,19 +7,18 @@ import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
 import factors.IAction;
+import factors.IStateVarBoolean;
+import factors.IStateVarDouble;
+import factors.IStateVarInt;
+import factors.IStateVarValue;
+import factors.StateVar;
 import mdp.StateVarTuple;
 import policy.Decision;
 import policy.Policy;
 
 public class PolicyWriter {
 
-	private FileWriter mWriter;
-
-	public PolicyWriter(String policyJsonFilename) throws IOException {
-		mWriter = new FileWriter(policyJsonFilename);
-	}
-
-	public void writePolicy(Policy policy) {
+	public void writePolicy(Policy policy, String policyJsonFilename) throws IOException {
 		JSONArray policyArray = new JSONArray();
 
 		for (Decision decision : policy) {
@@ -30,17 +29,40 @@ public class PolicyWriter {
 			decisionJsonObj.put("action", actionJsonObj);
 			policyArray.add(decisionJsonObj);
 		}
+
+		try (FileWriter writer = new FileWriter(policyJsonFilename)) {
+			writer.write(policyArray.toJSONString());
+			writer.flush();
+		}
 	}
 
 	private JSONObject writeState(StateVarTuple stateVarTuple) {
 		JSONObject stateJsonObj = new JSONObject();
-		// TODO
+		for (StateVar<IStateVarValue> stateVar : stateVarTuple) {
+			String varName = stateVar.getName();
+			IStateVarValue value = stateVar.getValue();
+			if (value instanceof IStateVarBoolean) {
+				IStateVarBoolean boolValue = (IStateVarBoolean) value;
+				stateJsonObj.put(varName, boolValue.getValue());
+			} else if (value instanceof IStateVarInt) {
+				IStateVarInt intValue = (IStateVarInt) value;
+				stateJsonObj.put(varName, intValue.getValue());
+			} else if (value instanceof IStateVarDouble) {
+				IStateVarDouble doubleValue = (IStateVarDouble) value;
+				stateJsonObj.put(varName, doubleValue.getValue());
+			} else {
+				stateJsonObj.put(varName, value.toString());
+			}
+		}
 		return stateJsonObj;
 	}
 
 	private JSONObject writeAction(IAction action) {
 		JSONObject actionJsonObj = new JSONObject();
-		// TODO
+		actionJsonObj.put("type", action.getNamePrefix());
+		JSONArray paramArray = new JSONArray();
+		paramArray.addAll(action.getParameters());
+		actionJsonObj.put("params", paramArray);
 		return actionJsonObj;
 	}
 }

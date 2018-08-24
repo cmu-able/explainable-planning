f4d526786662e7c996e9acbbefe1c41240b6410f
Roykrong Sukkerd <rsukkerd@andrew.cmu.edu>
2018-08-24 18:52:26 -0400

    Initial solver.gurobiconnector

---
+++ XPlanning/src/solver/gurobiconnector/ExplicitMDP.java
@@ -0,0 +1,16 @@
+package solver.gurobiconnector;
+
+public class ExplicitMDP {
+
+	public ExplicitMDP(int numStates, int numActions) {
+		// TODO
+	}
+
+	public void addTransitionProbability(int srcState, int destState, double probability, String actionName) {
+		// TODO
+	}
+
+	public void addTransitionCost(int srcState, int destState, double cost) {
+		// TODO
+	}
+}
---
+++ XPlanning/src/solver/gurobiconnector/UpperBoundOccupationMeasureSolver.java
@@ -0,0 +1,9 @@
+package solver.gurobiconnector;
+
+public class UpperBoundOccupationMeasureSolver {
+
+	public double getUpperBoundOccupationMeasure(ExplicitMDP explicitMDP) {
+		// TODO
+		return 0;
+	}
+}

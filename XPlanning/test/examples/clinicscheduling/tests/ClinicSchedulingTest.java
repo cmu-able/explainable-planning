512ff457bf6c5e593651a705472ddf542207e3e5
Roykrong Sukkerd <rsukkerd@andrew.cmu.edu>
2019-02-19 18:13:56 -0500

    Refactored domain-agnostic code from ClinicSchedulingTest

--- XPlanning/test/examples/clinicscheduling/tests/ClinicSchedulingTest.java
+++ XPlanning/test/examples/clinicscheduling/tests/ClinicSchedulingTest.java
@@ -1,9 +1,12 @@
 package examples.clinicscheduling.tests;
 
+import static org.testng.Assert.assertTrue;
+
 import java.io.File;
 import java.io.IOException;
+import java.util.Map;
+import java.util.Map.Entry;
 
-import org.apache.commons.io.FilenameUtils;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
@@ -11,133 +14,45 @@ import org.testng.annotations.Test;
 import examples.clinicscheduling.demo.ClinicSchedulingDemo;
 import examples.clinicscheduling.demo.ClinicSchedulingXMDPLoader;
 import examples.common.DSMException;
-import examples.common.Directories;
-import examples.utils.LPMCComparator;
+import examples.utils.LPMCComparisonTestUtis;
 import examples.utils.SimpleConsoleLogger;
 import examples.utils.XMDPDataProvider;
-import explanation.analysis.PolicyInfo;
 import gurobi.GRBException;
 import language.domain.metrics.IQFunction;
 import language.exceptions.XMDPException;
-import language.mdp.QSpace;
 import language.mdp.XMDP;
-import language.objectives.AttributeConstraint;
-import language.objectives.AttributeConstraint.BOUND_TYPE;
 import language.objectives.CostCriterion;
-import language.objectives.CostFunction;
-import language.policy.Policy;
 import prism.PrismException;
-import solver.gurobiconnector.GRBConnector;
-import solver.gurobiconnector.GRBConnectorSettings;
-import solver.gurobiconnector.GRBSolverUtils;
-import solver.prismconnector.PrismConnector;
-import solver.prismconnector.PrismConnectorSettings;
-import solver.prismconnector.ValueEncodingScheme;
-import solver.prismconnector.exceptions.ExplicitModelParsingException;
-import solver.prismconnector.exceptions.ResultParsingException;
-import solver.prismconnector.explicitmodel.PrismExplicitModelPointer;
-import solver.prismconnector.explicitmodel.PrismExplicitModelReader;
+import solver.prismconnector.exceptions.PrismConnectorException;
 
 public class ClinicSchedulingTest {
 	private static final double EQUALITY_TOL = 1e-6;
 
 	@Test(dataProvider = "xmdpProblems")
-	public void testUnconstrainedAverageCost(File problemFile, XMDP xmdp) throws PrismException, XMDPException,
-			IOException, ExplicitModelParsingException, GRBException, ResultParsingException {
-		// Problem file
-		String problemName = FilenameUtils.removeExtension(problemFile.getName());
-		String modelOutputPath = Directories.PRISM_MODELS_OUTPUT_PATH + "/" + problemName;
-		String advOutputPath = Directories.PRISM_ADVS_OUTPUT_PATH + "/" + problemName;
-
-		// Create PRISM connector
-		PrismConnectorSettings prismConnSetttings = new PrismConnectorSettings(modelOutputPath, advOutputPath);
-		PrismConnector prismConnector = new PrismConnector(xmdp, CostCriterion.AVERAGE_COST, prismConnSetttings);
-
-		// Export XMDP to explicit model files
-		PrismExplicitModelPointer prismExplicitModelPtr = prismConnector.exportExplicitModelFiles();
-		ValueEncodingScheme encodings = prismConnector.getPrismMDPTranslator().getValueEncodingScheme();
-
-		// Create PRISM explicit model reader
-		PrismExplicitModelReader prismExplicitModelReader = new PrismExplicitModelReader(prismExplicitModelPtr,
-				encodings);
-
-		// Create GRB connector
-		GRBConnectorSettings grbConnSettings = new GRBConnectorSettings(prismExplicitModelReader,
-				GRBSolverUtils.DEFAULT_FEASIBILITY_TOL, GRBSolverUtils.DEFAULT_INT_FEAS_TOL);
-		GRBConnector grbConnector = new GRBConnector(xmdp, CostCriterion.AVERAGE_COST, grbConnSettings);
-
-		LPMCComparator comparator = new LPMCComparator(grbConnector, prismConnector, EQUALITY_TOL);
-
-		// Compute an average-optimal policy using GRBSolver (LP method)
-		PolicyInfo solPolicyInfo = grbConnector.generateOptimalPolicy();
-		Policy solPolicy = solPolicyInfo.getPolicy();
-
-		// Compare policy evaluation between LP and MC methods
-		double[][] diffs = comparator.comparePolicyEvaluation(solPolicy, xmdp.getCostFunction(), xmdp.getQSpace());
-
-		// Print summary statistics
-		comparator.printSummaryStatistics(diffs);
-
-		// Close down PRISM
-		prismConnector.terminate();
+	public void testUnconstrainedAverageCost(File problemFile, XMDP xmdp)
+			throws PrismException, XMDPException, IOException, GRBException, PrismConnectorException {
+		double[][] diffs = LPMCComparisonTestUtis.compareUnconstrainedMDPCase(problemFile, xmdp, CostCriterion.AVERAGE_COST,
+				EQUALITY_TOL);
+		checkDifferences(diffs);
 	}
 
 	@Test(dataProvider = "xmdpProblems")
-	public void testConstrainedAverageCost(File problemFile, XMDP xmdp) throws PrismException, XMDPException,
-			IOException, ExplicitModelParsingException, GRBException, ResultParsingException {
-		CostFunction costFunction = xmdp.getCostFunction();
-		QSpace qFunctions = xmdp.getQSpace();
+	public void testConstrainedAverageCost(File problemFile, XMDP xmdp)
+			throws PrismException, XMDPException, IOException, GRBException, PrismConnectorException {
+		Map<IQFunction<?, ?>, double[][]> resultDiffs = LPMCComparisonTestUtis.compareConstrainedMDPCase(problemFile, xmdp,
+				CostCriterion.AVERAGE_COST, EQUALITY_TOL);
 
-		// Problem file
-		String problemName = FilenameUtils.removeExtension(problemFile.getName());
-		String modelOutputPath = Directories.PRISM_MODELS_OUTPUT_PATH + "/" + problemName;
-		String advOutputPath = Directories.PRISM_ADVS_OUTPUT_PATH + "/" + problemName;
-
-		// Create PRISM connector
-		PrismConnectorSettings prismConnSetttings = new PrismConnectorSettings(modelOutputPath, advOutputPath);
-		PrismConnector prismConnector = new PrismConnector(xmdp, CostCriterion.AVERAGE_COST, prismConnSetttings);
-
-		// Export XMDP to explicit model files
-		PrismExplicitModelPointer prismExplicitModelPtr = prismConnector.exportExplicitModelFiles();
-		ValueEncodingScheme encodings = prismConnector.getPrismMDPTranslator().getValueEncodingScheme();
-
-		// Create PRISM explicit model reader
-		PrismExplicitModelReader prismExplicitModelReader = new PrismExplicitModelReader(prismExplicitModelPtr,
-				encodings);
-
-		// Create GRB connector
-		GRBConnectorSettings grbConnSettings = new GRBConnectorSettings(prismExplicitModelReader,
-				GRBSolverUtils.DEFAULT_FEASIBILITY_TOL, GRBSolverUtils.DEFAULT_INT_FEAS_TOL);
-		GRBConnector grbConnector = new GRBConnector(xmdp, CostCriterion.AVERAGE_COST, grbConnSettings);
-
-		LPMCComparator comparator = new LPMCComparator(grbConnector, prismConnector, EQUALITY_TOL);
-
-		// Compute an average-optimal policy using GRBSolver (LP method)
-		PolicyInfo solPolicyInfo = grbConnector.generateOptimalPolicy();
-
-		for (IQFunction<?, ?> qFunction : qFunctions) {
-			double attrCostFuncSlope = costFunction.getAttributeCostFunction(qFunction).getSlope();
-
-			// Strict, hard upper/lower bound
-			BOUND_TYPE hardBoundType = attrCostFuncSlope > 0 ? BOUND_TYPE.STRICT_UPPER_BOUND
-					: BOUND_TYPE.STRICT_LOWER_BOUND;
-			double hardBoundValue = solPolicyInfo.getQAValue(qFunction);
-			AttributeConstraint<IQFunction<?, ?>> attrHardConstraint = new AttributeConstraint<IQFunction<?, ?>>(
-					qFunction, hardBoundType, hardBoundValue);
-
-			// Policy satisfying the QA constraint
-			PolicyInfo policyInfo = grbConnector.generateOptimalPolicy(costFunction, attrHardConstraint);
-			Policy policy = policyInfo.getPolicy();
-
-			// Compare policy evaluation between LP and MC methods
-			double[][] diffs = comparator.comparePolicyEvaluation(policy, costFunction, qFunctions);
-
-			// Print summary statistics
-			comparator.printSummaryStatistics(diffs);
+		for (Entry<IQFunction<?, ?>, double[][]> e : resultDiffs.entrySet()) {
+			double[][] diffs = e.getValue();
+			checkDifferences(diffs);
 		}
+	}
 
-		// Close down PRISM
-		prismConnector.terminate();
+	private void checkDifferences(double[][] diffs) {
+		assertTrue(LPMCComparisonTestUtis.allSmallQAValueDifferences(diffs, 1), "Large QA value difference");
+		assertTrue(LPMCComparisonTestUtis.allSmallPercentQAValueDifferences(diffs, 1),
+				"Large percentage QA value difference");
+		assertTrue(LPMCComparisonTestUtis.allSmallScaledQACostDifferences(diffs, 1), "Large scaled QA cost difference");
 	}
 
 	@DataProvider(name = "xmdpProblems")
---
+++ XPlanning/test/examples/utils/LPMCComparisonTestUtis.java
@@ -0,0 +1,175 @@
+package examples.utils;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.commons.io.FilenameUtils;
+
+import examples.common.Directories;
+import explanation.analysis.PolicyInfo;
+import gurobi.GRBException;
+import language.domain.metrics.IQFunction;
+import language.exceptions.XMDPException;
+import language.mdp.QSpace;
+import language.mdp.XMDP;
+import language.objectives.AttributeConstraint;
+import language.objectives.AttributeConstraint.BOUND_TYPE;
+import language.objectives.CostCriterion;
+import language.objectives.CostFunction;
+import language.policy.Policy;
+import prism.PrismException;
+import solver.gurobiconnector.GRBConnector;
+import solver.gurobiconnector.GRBConnectorSettings;
+import solver.gurobiconnector.GRBSolverUtils;
+import solver.prismconnector.PrismConnector;
+import solver.prismconnector.PrismConnectorSettings;
+import solver.prismconnector.ValueEncodingScheme;
+import solver.prismconnector.exceptions.PrismConnectorException;
+import solver.prismconnector.explicitmodel.PrismExplicitModelPointer;
+import solver.prismconnector.explicitmodel.PrismExplicitModelReader;
+
+public class LPMCComparisonTestUtis {
+
+	public static double[][] compareUnconstrainedMDPCase(File problemFile, XMDP xmdp, CostCriterion costCriterion,
+			double equalityTol)
+			throws PrismException, XMDPException, IOException, PrismConnectorException, GRBException {
+		// Problem file
+		String problemName = FilenameUtils.removeExtension(problemFile.getName());
+		String modelOutputPath = Directories.PRISM_MODELS_OUTPUT_PATH + "/" + problemName;
+		String advOutputPath = Directories.PRISM_ADVS_OUTPUT_PATH + "/" + problemName;
+
+		// Create PRISM connector
+		PrismConnectorSettings prismConnSetttings = new PrismConnectorSettings(modelOutputPath, advOutputPath);
+		PrismConnector prismConnector = new PrismConnector(xmdp, costCriterion, prismConnSetttings);
+
+		// Export XMDP to explicit model files
+		PrismExplicitModelPointer prismExplicitModelPtr = prismConnector.exportExplicitModelFiles();
+		ValueEncodingScheme encodings = prismConnector.getPrismMDPTranslator().getValueEncodingScheme();
+
+		// Create PRISM explicit model reader
+		PrismExplicitModelReader prismExplicitModelReader = new PrismExplicitModelReader(prismExplicitModelPtr,
+				encodings);
+
+		// Create GRB connector
+		GRBConnectorSettings grbConnSettings = new GRBConnectorSettings(prismExplicitModelReader,
+				GRBSolverUtils.DEFAULT_FEASIBILITY_TOL, GRBSolverUtils.DEFAULT_INT_FEAS_TOL);
+		GRBConnector grbConnector = new GRBConnector(xmdp, costCriterion, grbConnSettings);
+
+		LPMCComparator comparator = new LPMCComparator(grbConnector, prismConnector, equalityTol);
+
+		// Compute an optimal policy for unconstrained MDP
+		PolicyInfo solPolicyInfo = null;
+
+		if (costCriterion == CostCriterion.AVERAGE_COST) {
+			solPolicyInfo = grbConnector.generateOptimalPolicy();
+		} else if (costCriterion == CostCriterion.TOTAL_COST) {
+			solPolicyInfo = prismConnector.generateOptimalPolicy();
+		}
+
+		Policy solPolicy = solPolicyInfo.getPolicy();
+
+		// Compare policy evaluation between LP and MC methods
+		double[][] diffs = comparator.comparePolicyEvaluation(solPolicy, xmdp.getCostFunction(), xmdp.getQSpace());
+
+		// Print summary statistics
+		comparator.printSummaryStatistics(diffs);
+
+		// Close down PRISM
+		prismConnector.terminate();
+
+		return diffs;
+	}
+
+	public static Map<IQFunction<?, ?>, double[][]> compareConstrainedMDPCase(File problemFile, XMDP xmdp,
+			CostCriterion costCriterion, double equalityTol)
+			throws PrismException, XMDPException, IOException, PrismConnectorException, GRBException {
+		CostFunction costFunction = xmdp.getCostFunction();
+		QSpace qFunctions = xmdp.getQSpace();
+
+		// Problem file
+		String problemName = FilenameUtils.removeExtension(problemFile.getName());
+		String modelOutputPath = Directories.PRISM_MODELS_OUTPUT_PATH + "/" + problemName;
+		String advOutputPath = Directories.PRISM_ADVS_OUTPUT_PATH + "/" + problemName;
+
+		// Create PRISM connector
+		PrismConnectorSettings prismConnSetttings = new PrismConnectorSettings(modelOutputPath, advOutputPath);
+		PrismConnector prismConnector = new PrismConnector(xmdp, costCriterion, prismConnSetttings);
+
+		// Export XMDP to explicit model files
+		PrismExplicitModelPointer prismExplicitModelPtr = prismConnector.exportExplicitModelFiles();
+		ValueEncodingScheme encodings = prismConnector.getPrismMDPTranslator().getValueEncodingScheme();
+
+		// Create PRISM explicit model reader
+		PrismExplicitModelReader prismExplicitModelReader = new PrismExplicitModelReader(prismExplicitModelPtr,
+				encodings);
+
+		// Create GRB connector
+		GRBConnectorSettings grbConnSettings = new GRBConnectorSettings(prismExplicitModelReader,
+				GRBSolverUtils.DEFAULT_FEASIBILITY_TOL, GRBSolverUtils.DEFAULT_INT_FEAS_TOL);
+		GRBConnector grbConnector = new GRBConnector(xmdp, costCriterion, grbConnSettings);
+
+		LPMCComparator comparator = new LPMCComparator(grbConnector, prismConnector, equalityTol);
+
+		// Compute an optimal policy for unconstrained MDP
+		PolicyInfo solPolicyInfo = null;
+
+		if (costCriterion == CostCriterion.AVERAGE_COST) {
+			solPolicyInfo = grbConnector.generateOptimalPolicy();
+		} else if (costCriterion == CostCriterion.TOTAL_COST) {
+			solPolicyInfo = prismConnector.generateOptimalPolicy();
+		}
+
+		Map<IQFunction<?, ?>, double[][]> resultDiffs = new HashMap<>();
+
+		for (IQFunction<?, ?> qFunction : qFunctions) {
+			double attrCostFuncSlope = costFunction.getAttributeCostFunction(qFunction).getSlope();
+
+			// Strict, hard upper/lower bound
+			BOUND_TYPE hardBoundType = attrCostFuncSlope > 0 ? BOUND_TYPE.STRICT_UPPER_BOUND
+					: BOUND_TYPE.STRICT_LOWER_BOUND;
+			double hardBoundValue = solPolicyInfo.getQAValue(qFunction);
+			AttributeConstraint<IQFunction<?, ?>> attrHardConstraint = new AttributeConstraint<IQFunction<?, ?>>(
+					qFunction, hardBoundType, hardBoundValue);
+
+			// Compute a constraint-satisfying optimal policy -- only GRBSolver supports this
+			PolicyInfo policyInfo = grbConnector.generateOptimalPolicy(costFunction, attrHardConstraint);
+			Policy policy = policyInfo.getPolicy();
+
+			// Compare policy evaluation between LP and MC methods
+			double[][] diffs = comparator.comparePolicyEvaluation(policy, costFunction, qFunctions);
+
+			// Print summary statistics
+			comparator.printSummaryStatistics(diffs);
+		}
+
+		// Close down PRISM
+		prismConnector.terminate();
+
+		return resultDiffs;
+	}
+
+	public static boolean allSmallQAValueDifferences(double[][] diffs, double threshold) {
+		return allSmallValueDifferences(diffs, threshold, 0);
+	}
+
+	public static boolean allSmallPercentQAValueDifferences(double[][] diffs, double threshold) {
+		return allSmallValueDifferences(diffs, threshold, 1);
+	}
+
+	public static boolean allSmallScaledQACostDifferences(double[][] diffs, double threshold) {
+		return allSmallValueDifferences(diffs, threshold, 2);
+	}
+
+	private static boolean allSmallValueDifferences(double[][] diffs, double threshold, int valueType) {
+		// Excluding the element at index 0 since we don't compute its value
+		for (int k = 1; k < diffs.length; k++) {
+			double valueDiff = diffs[k][valueType];
+			if (valueDiff > threshold) {
+				return false;
+			}
+		}
+		return true;
+	}
+}

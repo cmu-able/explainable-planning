c04cbfdde9b2f1cf17667d45412c567209facb86
Roykrong Sukkerd <rsukkerd@andrew.cmu.edu>
2019-03-20 14:52:25 -0400

    Write node/edge attributes to JSON in convertMapTopologyToJSONObject()

--- XPlanningEvaluation/src/mobilerobot/utilities/MapTopologyUtils.java
+++ XPlanningEvaluation/src/mobilerobot/utilities/MapTopologyUtils.java
@@ -4,8 +4,11 @@ import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
@@ -16,7 +19,7 @@ import examples.mobilerobot.dsm.IEdgeAttribute;
 import examples.mobilerobot.dsm.INodeAttribute;
 import examples.mobilerobot.dsm.LocationNode;
 import examples.mobilerobot.dsm.MapTopology;
-import examples.mobilerobot.dsm.exceptions.LocationNodeNotFoundException;
+import examples.mobilerobot.dsm.exceptions.ConnectionAttributeNotFoundException;
 import examples.mobilerobot.dsm.exceptions.MapTopologyException;
 import examples.mobilerobot.dsm.parser.AreaParser;
 import examples.mobilerobot.dsm.parser.IEdgeAttributeParser;
@@ -57,23 +60,32 @@ public class MapTopologyUtils {
 		return mapReader.readMapTopology(mapJsonFile, defaultNodeAttributes, defaultEdgeAttributes);
 	}
 
-	public static JSONObject convertMapTopologyToJSONObject(MapTopology mapTopology)
-			throws LocationNodeNotFoundException {
+	public static JSONObject convertMapTopologyToJSONObject(MapTopology mapTopology) throws MapTopologyException {
 		JSONObject mapJSONObj = new JSONObject();
-		mapJSONObj.put("mur", 10); // use default MUR value = 10
 
+		// mur
+		mapJSONObj.put("mur", 10); // use default meter-unit ratio value = 10
+
+		// map
 		JSONArray nodesJSONArray = new JSONArray();
-
 		for (LocationNode locNode : mapTopology) {
 			JSONObject nodeJSONObj = convertLocationNodeToJSONObject(locNode, mapTopology);
 			nodesJSONArray.add(nodeJSONObj);
 		}
 		mapJSONObj.put("map", nodesJSONArray);
+
+		// obstacles
+		ConnectionAttributeFilter<Occlusion> filter = new ConnectionAttributeFilter<>("occlusion",
+				Occlusion.PARTIALLY_OCCLUDED, Occlusion.BLOCKED);
+		JSONArray obstacleJSONArray = convertConnectionAttributesToJSONArray(mapTopology.connectionIterator(),
+				"occlusion", filter);
+		mapJSONObj.put("obstacles", obstacleJSONArray);
+
 		return mapJSONObj;
 	}
 
 	private static JSONObject convertLocationNodeToJSONObject(LocationNode locNode, MapTopology mapTopology)
-			throws LocationNodeNotFoundException {
+			throws MapTopologyException {
 		JSONObject nodeJSONObj = new JSONObject();
 		nodeJSONObj.put("node-id", locNode.getNodeID());
 
@@ -88,6 +100,53 @@ public class MapTopologyUtils {
 			connJSONArray.add(otherLocNode.getNodeID());
 		}
 		nodeJSONObj.put("connected-to", connJSONArray);
+
+		Area area = locNode.getNodeAttribute(Area.class, "area");
+		nodeJSONObj.put("area", area.toString());
+
 		return nodeJSONObj;
+	}
+
+	private static <E extends IEdgeAttribute> JSONArray convertConnectionAttributesToJSONArray(
+			Iterator<Connection> iter, String attributeName, ConnectionAttributeFilter<E> filter)
+			throws ConnectionAttributeNotFoundException {
+		JSONArray connAttrJSONArray = new JSONArray();
+		while (iter.hasNext()) {
+			Connection connection = iter.next();
+			if (filter.filter(connection)) {
+				JSONObject connAttrJSONObj = convertConnectionAttributeToJSONObject(connection, attributeName);
+				connAttrJSONArray.add(connAttrJSONObj);
+			}
+		}
+		return connAttrJSONArray;
+	}
+
+	private static JSONObject convertConnectionAttributeToJSONObject(Connection connection, String attributeName)
+			throws ConnectionAttributeNotFoundException {
+		JSONObject connAttrJSONObj = new JSONObject();
+		connAttrJSONObj.put("from-id", connection.getNodeA().getNodeID());
+		connAttrJSONObj.put("to-id", connection.getNodeB().getNodeID());
+
+		IEdgeAttribute connAttribute = connection.getGenericConnectionAttribute(attributeName);
+		connAttrJSONObj.put(attributeName, connAttribute.toString());
+
+		return connAttrJSONObj;
+	}
+
+	private static class ConnectionAttributeFilter<E extends IEdgeAttribute> {
+
+		private String mAttributeName;
+
+		private Set<E> mAttributeFilter;
+
+		ConnectionAttributeFilter(String attributeName, E... attributes) {
+			mAttributeName = attributeName;
+			mAttributeFilter = Stream.of(attributes).collect(Collectors.toSet());
+		}
+
+		public boolean filter(Connection connection) throws ConnectionAttributeNotFoundException {
+			IEdgeAttribute connAttr = connection.getGenericConnectionAttribute(mAttributeName);
+			return mAttributeFilter.contains(connAttr);
+		}
 	}
 }

9e8cabf7ece1a927037526043a74958937458479
Roykrong Sukkerd <rsukkerd@andrew.cmu.edu>
2019-03-20 11:50:52 -0400

    Added convertMapTopologyToJSONObject() to MapTopologyUtils

--- XPlanningEvaluation/src/mobilerobot/utilities/MapTopologyUtils.java
+++ XPlanningEvaluation/src/mobilerobot/utilities/MapTopologyUtils.java
@@ -7,11 +7,16 @@ import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 import org.json.simple.parser.ParseException;
 
+import examples.mobilerobot.dsm.Connection;
 import examples.mobilerobot.dsm.IEdgeAttribute;
 import examples.mobilerobot.dsm.INodeAttribute;
+import examples.mobilerobot.dsm.LocationNode;
 import examples.mobilerobot.dsm.MapTopology;
+import examples.mobilerobot.dsm.exceptions.LocationNodeNotFoundException;
 import examples.mobilerobot.dsm.exceptions.MapTopologyException;
 import examples.mobilerobot.dsm.parser.AreaParser;
 import examples.mobilerobot.dsm.parser.IEdgeAttributeParser;
@@ -50,5 +55,39 @@ public class MapTopologyUtils {
 			mapReader = new MapTopologyReader(new HashSet<>(), new HashSet<>());
 		}
 		return mapReader.readMapTopology(mapJsonFile, defaultNodeAttributes, defaultEdgeAttributes);
+	}
+
+	public static JSONObject convertMapTopologyToJSONObject(MapTopology mapTopology)
+			throws LocationNodeNotFoundException {
+		JSONObject mapJSONObj = new JSONObject();
+		mapJSONObj.put("mur", 10); // use default MUR value = 10
+
+		JSONArray nodesJSONArray = new JSONArray();
+
+		for (LocationNode locNode : mapTopology) {
+			JSONObject nodeJSONObj = convertLocationNodeToJSONObject(locNode, mapTopology);
+			nodesJSONArray.add(nodeJSONObj);
+		}
+		mapJSONObj.put("map", nodesJSONArray);
+		return mapJSONObj;
+	}
+
+	private static JSONObject convertLocationNodeToJSONObject(LocationNode locNode, MapTopology mapTopology)
+			throws LocationNodeNotFoundException {
+		JSONObject nodeJSONObj = new JSONObject();
+		nodeJSONObj.put("node-id", locNode.getNodeID());
+
+		JSONObject coordsJSONObj = new JSONObject();
+		coordsJSONObj.put("x", locNode.getNodeXCoordinate());
+		coordsJSONObj.put("y", locNode.getNodeYCoordinate());
+		nodeJSONObj.put("coords", coordsJSONObj);
+
+		JSONArray connJSONArray = new JSONArray();
+		for (Connection connection : mapTopology.getConnections(locNode)) {
+			LocationNode otherLocNode = connection.getOtherNode(locNode);
+			connJSONArray.add(otherLocNode.getNodeID());
+		}
+		nodeJSONObj.put("connected-to", connJSONArray);
+		return nodeJSONObj;
 	}
 }

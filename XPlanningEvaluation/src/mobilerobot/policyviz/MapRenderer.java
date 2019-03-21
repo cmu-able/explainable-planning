95c694b0ff33a746d80a87d8fa7889f59d30ed71
Roykrong Sukkerd <rsukkerd@andrew.cmu.edu>
2019-03-21 16:21:55 -0400

    Map and Policy renderers

---
+++ XPlanningEvaluation/src/mobilerobot/policyviz/MapRenderer.java
@@ -0,0 +1,45 @@
+package mobilerobot.policyviz;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FilenameUtils;
+import org.json.simple.parser.ParseException;
+
+import examples.mobilerobot.dsm.exceptions.MapTopologyException;
+import guru.nidi.graphviz.model.MutableGraph;
+import mobilerobot.utilities.FileIOUtils;
+
+public class MapRenderer {
+
+	private GraphVizRenderer mGraphRenderer;
+
+	public MapRenderer(double meterPerInch, double scalingFactor) {
+		mGraphRenderer = new GraphVizRenderer(meterPerInch, scalingFactor);
+	}
+
+	public void render(File mapJsonFile) throws MapTopologyException, IOException, ParseException {
+		MapJSONToGraphViz mapToGraph = new MapJSONToGraphViz(mapJsonFile, mGraphRenderer);
+		MutableGraph mapGraph = mapToGraph.convertMapJsonToGraph();
+		String outputName = FilenameUtils.removeExtension(mapJsonFile.getName());
+		GraphVizRenderer.drawGraph(mapGraph, outputName);
+	}
+
+	public static void main(String[] args) throws MapTopologyException, IOException, ParseException {
+		String mapsPath;
+		if (args.length > 0) {
+			mapsPath = args[0];
+		} else {
+			mapsPath = FileIOUtils.MAPS_RESOURCE_PATH;
+		}
+
+		File mapsDir = new File(mapsPath);
+		File[] mapJsonFiles = mapsDir.listFiles();
+		MapRenderer mapRenderer = new MapRenderer(GraphVizRenderer.METER_PER_INCH, GraphVizRenderer.SCALING_FACTOR);
+
+		for (File mapJsonFile : mapJsonFiles) {
+			mapRenderer.render(mapJsonFile);
+		}
+	}
+
+}
---
+++ XPlanningEvaluation/src/mobilerobot/policyviz/PolicyRenderer.java
@@ -0,0 +1,51 @@
+package mobilerobot.policyviz;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+import org.apache.commons.io.FilenameUtils;
+import org.json.simple.parser.ParseException;
+
+import examples.mobilerobot.dsm.exceptions.MapTopologyException;
+import examples.mobilerobot.dsm.exceptions.NodeIDNotFoundException;
+import guru.nidi.graphviz.model.MutableGraph;
+import mobilerobot.utilities.FileIOUtils;
+
+public class PolicyRenderer {
+
+	private PolicyJSONToGraphViz mPolicyToGraph;
+
+	public PolicyRenderer(File mapJsonFile, double meterPerInch, double scalingFactor)
+			throws MapTopologyException, IOException, ParseException {
+		GraphVizRenderer graphRenderer = new GraphVizRenderer(meterPerInch, scalingFactor);
+		mPolicyToGraph = new PolicyJSONToGraphViz(mapJsonFile, graphRenderer);
+	}
+
+	public void render(File policyJsonFile) throws NodeIDNotFoundException, IOException, ParseException {
+		MutableGraph policyGraph = mPolicyToGraph.convertPolicyJsonToGraph(policyJsonFile);
+		String outputName = FilenameUtils.removeExtension(policyJsonFile.getName());
+		GraphVizRenderer.drawGraph(policyGraph, outputName);
+	}
+
+	public static void main(String[] args)
+			throws URISyntaxException, MapTopologyException, IOException, ParseException {
+		String mapJsonFilename = args[0];
+		File mapJsonFile = FileIOUtils.getFile(PolicyRenderer.class, FileIOUtils.MAPS_RESOURCE_PATH, mapJsonFilename);
+		String policiesPath;
+		if (args.length > 1) {
+			policiesPath = args[1];
+		} else {
+			policiesPath = FileIOUtils.POLICIES_RESOURCE_PATH;
+		}
+
+		File policiesDir = new File(policiesPath);
+		File[] policyJsonFiles = policiesDir.listFiles();
+		PolicyRenderer policyRenderer = new PolicyRenderer(mapJsonFile, GraphVizRenderer.METER_PER_INCH,
+				GraphVizRenderer.SCALING_FACTOR);
+
+		for (File policyJsonFile : policyJsonFiles) {
+			policyRenderer.render(policyJsonFile);
+		}
+	}
+}

b0826c57f6d8ba26e748d865e5fb967833dd3d4d
Roykrong Sukkerd <rsukkerd@andrew.cmu.edu>
2019-04-09 18:03:00 -0400

    Constants: keep offset low

--- XPlanning/src/solver/common/Constants.java
+++ XPlanning/src/solver/common/Constants.java
@@ -8,7 +8,7 @@ public class Constants {
 	 * 
 	 * The cost offset is used in SSPs to ensure that all objective costs are positive, except in the goal states.
 	 */
-	public static final double SSP_COST_OFFSET = 1e-3;
+	public static final double SSP_COST_OFFSET = 1e-4;
 
 	private Constants() {
 		throw new IllegalStateException("Utility class");

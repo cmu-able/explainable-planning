b96af94f819ce2c54ea7d3648ce6af02b23b1e2b
Roykrong Sukkerd <rsukkerd@andrew.cmu.edu>
2019-04-09 16:24:05 -0400

    Updated comments and offset value in Constants

--- XPlanning/src/solver/common/Constants.java
+++ XPlanning/src/solver/common/Constants.java
@@ -6,9 +6,9 @@ public class Constants {
 	 * This is to ensure that there is no zero-reward cycle in the MDP. This is because the current version of PRISM 4.4
 	 * does not support "constructing a strategy for Rmin in the presence of zero-reward ECs".
 	 * 
-	 * The cost offset is used in SSPs to ensure that solution policies have no cycles and reach a goal.
+	 * The cost offset is used in SSPs to ensure that all objective costs are positive, except in the goal states.
 	 */
-	public static final double SSP_COST_OFFSET = 1e-5;
+	public static final double SSP_COST_OFFSET = 1e-3;
 
 	private Constants() {
 		throw new IllegalStateException("Utility class");

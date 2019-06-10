89210685a0b521e0ca430be5b2e5aee4252f18c6
Roykrong Sukkerd <rsukkerd@andrew.cmu.edu>
2019-06-10 17:30:50 -0400

    ExplanationHTMLGenerator: set layout for explanation

--- XPlanningEvaluation/src/mobilerobot/study/utilities/ExplanationHTMLGenerator.java
+++ XPlanningEvaluation/src/mobilerobot/study/utilities/ExplanationHTMLGenerator.java
@@ -22,16 +22,20 @@ public class ExplanationHTMLGenerator {
 
 	private static final double POLICY_IMG_WIDTH_TO_HEIGHT_RATIO = 0.47;
 	private static final int DEFAULT_POLICY_IMG_WIDTH_PX = 500;
+	private static final String W3_CONTAINER = "w3-container";
+	private static final String W3_CELL = "w3-cell";
+	private static final String W3_CENTER = "w3-center";
 
 	private double mPolicyImgWidthToHeightRatio;
 	private int mPolicyImgWidthPx;
+	private Pattern mJsonFileRefPattern = Pattern.compile("(\\[([^\\[]+)\\.json\\])");
 
 	public ExplanationHTMLGenerator(double policyImgWidthToHeightRatio, int policyImgWidthPx) {
 		mPolicyImgWidthToHeightRatio = policyImgWidthToHeightRatio;
 		mPolicyImgWidthPx = policyImgWidthPx;
 	}
 
-	public void createExplanationHTMLFile(File explanationJsonFile, File outDir) throws IOException, ParseException {
+	public void createExplanationHTMLFileBasic(File explanationJsonFile, File outDir) throws IOException, ParseException {
 		JSONObject explanationJsonObj = FileIOUtils.readJSONObjectFromFile(explanationJsonFile);
 		String explanationText = (String) explanationJsonObj.get("Explanation");
 
@@ -46,8 +50,7 @@ public class ExplanationHTMLGenerator {
 		int heightPx = (int) Math.round(widthPx / mPolicyImgWidthToHeightRatio);
 		String imgHTMLElementStr = "<img src=\"%s\" width=\"%d\" height=\"%d\">";
 
-		Pattern jsonFileRefPattern = Pattern.compile("(\\[([^\\[]+)\\.json\\])");
-		Matcher matcher = jsonFileRefPattern.matcher(explanationHTMLStr);
+		Matcher matcher = mJsonFileRefPattern.matcher(explanationHTMLStr);
 		while (matcher.find()) {
 			String jsonFileRef = matcher.group(1); // [/path/to/policyX.json]
 			String pngFullFilename = matcher.group(2) + ".png"; // /path/to/policyX.png
@@ -59,6 +62,95 @@ public class ExplanationHTMLGenerator {
 		String explanationHTMLFilename = FilenameUtils.removeExtension(explanationJsonFile.getName()) + ".html";
 		Path explanationHTMLPath = outDir.toPath().resolve(explanationHTMLFilename);
 		Files.write(explanationHTMLPath, explanationHTMLWithImages.getBytes());
+	}
+
+	public void createExplanationHTMLFile(File explanationJsonFile, File outDir) throws IOException, ParseException {
+		JSONObject explanationJsonObj = FileIOUtils.readJSONObjectFromFile(explanationJsonFile);
+		String explanationText = (String) explanationJsonObj.get("Explanation");
+
+		Document doc = createExplanationDocument(explanationText);
+		String explanationHTML = doc.toString();
+
+		String explanationHTMLFilename = FilenameUtils.removeExtension(explanationJsonFile.getName()) + ".html";
+		Path explanationHTMLPath = outDir.toPath().resolve(explanationHTMLFilename);
+		Files.write(explanationHTMLPath, explanationHTML.getBytes());
+	}
+
+	private Document createExplanationDocument(String explanationText) {
+		Document doc = Jsoup.parse("<html></html>");
+		// <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+		doc.appendElement("link").attr("rel", "stylesheet").attr("href", "https://www.w3schools.com/w3css/4/w3.css");
+
+		String[] parts = explanationText.split("\n\n");
+		for (int i = 0; i < parts.length; i++) {
+			String policyExplanation = parts[i];
+			int imgIndex = i + 1;
+
+			Element policySectionDiv = createPolicySectionDiv(policyExplanation, imgIndex);
+			doc.appendChild(policySectionDiv);
+		}
+		return doc;
+	}
+
+	private Element createPolicySectionDiv(String policyExplanation, int imgIndex) {
+		Element container = new Element("div");
+		container.addClass(W3_CONTAINER);
+		if (Math.floorMod(imgIndex, 2) == 0) {
+			container.addClass("w3-pale-yellow");
+		} else {
+			container.addClass("w3-light-grey");
+		}
+
+		int widthPx = mPolicyImgWidthPx;
+		int heightPx = (int) Math.round(widthPx / mPolicyImgWidthToHeightRatio);
+		String pngFilename = null;
+		String policyExplanationWithImgRef = null;
+
+		Matcher matcher = mJsonFileRefPattern.matcher(policyExplanation);
+		if (matcher.find()) {
+			String jsonFileRef = matcher.group(1); // [/path/to/policyX.json]
+			String pngFullFilename = matcher.group(2) + ".png"; // /path/to/policyX.png
+			pngFilename = FilenameUtils.getName(pngFullFilename);
+			policyExplanationWithImgRef = policyExplanation.replace(jsonFileRef, "(see Figure " + imgIndex + ")");
+		}
+
+		Element policyImgDiv = createPolicyImgDiv(pngFilename, widthPx, heightPx, imgIndex);
+		Element policyExplanationDiv = createPolicyExplanationDiv(policyExplanationWithImgRef);
+
+		container.appendChild(policyImgDiv);
+		container.appendChild(policyExplanationDiv);
+		return container;
+	}
+
+	private Element createPolicyImgDiv(String pngFilename, int widthPx, int heightPx, int imgIndex) {
+		Element container = new Element("div");
+		container.addClass(W3_CONTAINER);
+		container.addClass(W3_CELL);
+		container.addClass(W3_CENTER);
+
+		Element policyImg = new Element("img");
+		policyImg.attr("src", pngFilename);
+		policyImg.attr("width", Integer.toString(widthPx));
+		policyImg.attr("height", Integer.toString(heightPx));
+
+		Element policyImgCaption = new Element("h5");
+		policyImgCaption.text("Figure " + imgIndex);
+
+		container.appendChild(policyImg);
+		container.appendChild(policyImgCaption);
+		return container;
+	}
+
+	private Element createPolicyExplanationDiv(String policyExplanationWithImgRef) {
+		Element container = new Element("div");
+		container.addClass(W3_CONTAINER);
+		container.addClass(W3_CELL);
+
+		Element policyExplanationP = new Element("p");
+		policyExplanationP.text(policyExplanationWithImgRef);
+
+		container.appendChild(policyExplanationP);
+		return container;
 	}
 
 	public void createAllExplanationHTMLFiles(File rootDir) throws IOException, ParseException {

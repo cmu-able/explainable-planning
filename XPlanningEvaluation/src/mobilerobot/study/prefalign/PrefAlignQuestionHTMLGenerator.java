9dcd265cbe80fe0ae587a99daf2ea40719ab8c27
Roykrong Sukkerd <rsukkerd@andrew.cmu.edu>
2019-06-24 16:10:57 -0400

    PrefAlignQuestionHTMLGenerator: createCostStructureTable() and some refactoring

--- XPlanningEvaluation/src/mobilerobot/study/prefalign/PrefAlignQuestionHTMLGenerator.java
+++ XPlanningEvaluation/src/mobilerobot/study/prefalign/PrefAlignQuestionHTMLGenerator.java
@@ -14,35 +14,51 @@ import mobilerobot.utilities.FileIOUtils;
 
 public class PrefAlignQuestionHTMLGenerator {
 
-	public PrefAlignQuestionHTMLGenerator() {
+	private static final String MISSION_TEXT = "Find the best policy from %s to %s that minimizes the following costs:";
+	private static final String JSON_EXTENSION = ".json";
 
+	public PrefAlignQuestionHTMLGenerator() {
+		//
 	}
 
-	public void createPrefAlignQuestionHTMLFile(File missionJsonFile, int agentIndex, File questionDir)
-			throws IOException, ParseException {
+	public void createPrefAlignQuestionHTMLFile(File questionDir, int agentIndex) throws IOException, ParseException {
+		// There is only 1 missionX.json and 1 simpleCostStructure.json per question dir
+		File missionJsonFile = FileIOUtils.listFilesWithRegexFilter(questionDir, "mission[0-9]+", JSON_EXTENSION)[0];
+		File costStructJsonFile = FileIOUtils.listFilesWithFilter(questionDir, "simpleCostStructure",
+				JSON_EXTENSION)[0];
+
+		// Only 1 agentPolicy[agentIndex].png and 1 agentPolicyValues[agentIndex].json per question dir
+		File agentPolicyPngFile = FileIOUtils.listFilesWithFilter(questionDir, "agentPolicy" + agentIndex, ".png")[0];
+		File agentPolicyValuesJsonFile = FileIOUtils.listFilesWithFilter(questionDir, "agentPolicyValues" + agentIndex,
+				JSON_EXTENSION)[0];
+
 		JSONObject missionJsonObj = FileIOUtils.readJSONObjectFromFile(missionJsonFile);
-		Document questionDoc = createPrefAlignQuestionDocument(missionJsonObj, agentIndex);
+		JSONObject costStructJsonObj = FileIOUtils.readJSONObjectFromFile(costStructJsonFile);
+		Document questionDoc = createPrefAlignQuestionDocument(missionJsonObj, costStructJsonObj, agentPolicyPngFile,
+				agentPolicyValuesJsonFile);
 		String questionDocName = null;
 		HTMLGeneratorUtils.writeHTMLDocumentToFile(questionDoc, questionDocName, questionDir);
 	}
 
-	private Document createPrefAlignQuestionDocument(JSONObject missionJsonObj, int agentIndex) {
+	private Document createPrefAlignQuestionDocument(JSONObject missionJsonObj, JSONObject costStructJsonObj,
+			File agentPolicyPngFile, File agentPolicyValuesJsonFile) {
 		Document doc = HTMLGeneratorUtils.createHTMLBlankDocument();
 
-		Element missionQuestionDiv = createMissionQuestionDiv(missionJsonObj);
+		Element missionQuestionDiv = createMissionQuestionDiv(missionJsonObj, costStructJsonObj);
 		doc.body().appendChild(missionQuestionDiv);
 
-		Element agentProposalQuestionDiv = createAgentProposalQuestionDiv();
+		Element agentProposalQuestionDiv = createAgentProposalQuestionDiv(agentPolicyPngFile,
+				agentPolicyValuesJsonFile);
 		doc.body().appendChild(agentProposalQuestionDiv);
 
 		return doc;
 	}
 
-	private Element createMissionQuestionDiv(JSONObject missionJsonObj) {
+	private Element createMissionQuestionDiv(JSONObject missionJsonObj, JSONObject costStructJsonObj) {
 		String mapJsonFilename = (String) missionJsonObj.get("map-file");
 
 		// Mission paragraph and table
-		Element missionDiv = createMissionDiv(missionJsonObj);
+		Element missionDiv = createMissionDiv(missionJsonObj, costStructJsonObj);
 
 		// Map image
 		String mapPngFilename = FilenameUtils.removeExtension(mapJsonFilename) + ".png";
@@ -55,21 +71,53 @@ public class PrefAlignQuestionHTMLGenerator {
 		return container;
 	}
 
-	private Element createMissionDiv(JSONObject missionJsonObj) {
+	private Element createMissionDiv(JSONObject missionJsonObj, JSONObject costStructJsonObj) {
+		Element container = HTMLGeneratorUtils.createBlankContainer(HTMLGeneratorUtils.W3_TWOTHIRD);
+
 		String startID = (String) missionJsonObj.get("start-id");
 		String goalID = (String) missionJsonObj.get("goal-id");
 
 		// Mission paragraph
+		String missionInstr = String.format(MISSION_TEXT, startID, goalID);
 		Element missionP = new Element("p");
-		missionP.text("");
+		missionP.text(missionInstr);
 
 		// Cost-structure table
-		// TODO
+		Element costStructTable = createCostStructureTable(costStructJsonObj);
 
-		return null;
+		container.appendChild(missionP);
+		container.appendChild(costStructTable);
+
+		return container;
 	}
 
-	private Element createAgentProposalQuestionDiv() {
+	private Element createCostStructureTable(JSONObject costStructJsonObj) {
+		Element table = HTMLGeneratorUtils.createResponsiveBlankTable();
+		// Header row
+		Element headerRow = table.appendElement("tr");
+
+		// Empty header for quality-attribute unit
+		headerRow.appendElement("th");
+
+		// Header for cost
+		headerRow.appendElement("th").addClass("w3-right-align").text("Cost ($)");
+
+		for (Object keyObj : costStructJsonObj.keySet()) {
+			String qaName = (String) keyObj;
+			JSONObject unitCostJsonObj = (JSONObject) costStructJsonObj.get(qaName);
+			String descriptiveUnit = (String) unitCostJsonObj.get("unit");
+			String formattedCost = (String) unitCostJsonObj.get("cost");
+
+			// Row for each QA unit cost
+			Element qaUnitCostRow = table.appendElement("tr");
+			qaUnitCostRow.appendElement("td").text(descriptiveUnit);
+			qaUnitCostRow.appendElement("td").addClass("w3-right-align").text("$" + formattedCost);
+		}
+
+		return table;
+	}
+
+	private Element createAgentProposalQuestionDiv(File agentPolicyPngFile, File agentPolicyValuesJsonFile) {
 		Element container = HTMLGeneratorUtils.createBlankContainerFullViewportHeight();
 		return container;
 	}

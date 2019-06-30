de26e3b7dcaa06184238a110894a42deeae2e9b2
Roykrong Sukkerd <rsukkerd@andrew.cmu.edu>
2019-06-30 14:48:19 -0400

    MTurkHTMLQuestionUtils

---
+++ XPlanningEvaluation/src/mobilerobot/study/utilities/MTurkHTMLQuestionUtils.java
@@ -0,0 +1,85 @@
+package mobilerobot.study.utilities;
+
+import org.jsoup.nodes.Element;
+
+public class MTurkHTMLQuestionUtils {
+
+	private MTurkHTMLQuestionUtils() {
+		throw new IllegalStateException("Utility class");
+	}
+
+	public static Element getCrowdHTMLScript() {
+		Element crowdHTMLScript = new Element("script");
+		crowdHTMLScript.attr("src", "https://assets.crowd.aws/crowd-html-elements.js");
+		return crowdHTMLScript;
+	}
+
+	public static Element getBlankCrowdFormContainer() {
+		Element container = new Element("div");
+		container.addClass("w3-container");
+		container.addClass("w3-margin");
+		container.addClass("w3-sand");
+		container.addClass("w3-card");
+
+		Element crowdForm = new Element("crowd-form");
+		container.appendChild(crowdForm);
+		return container;
+	}
+
+	public static Element getPrefAlignCrowdQuestionContainer() {
+		Element questionContainer = getBlankQuestionContainer(
+				"Is the agent's proposed policy the best one, with respect to the given cost profile?");
+		String[] optionNames = { "answer-yes", "answer-no" };
+		String[] optionLabels = { "Yes", "No" };
+		Element answerOptions = createCrowdRadioGroup(optionNames, optionLabels);
+		questionContainer.appendChild(answerOptions);
+		return questionContainer;
+	}
+
+	public static Element getJustificationCrowdQuestionContainer() {
+		Element questionContainer = getBlankQuestionContainer("Please provide justification for your answer:");
+		Element crowdTextArea = new Element("crowd-text-area");
+		crowdTextArea.attr("name", "justification");
+		crowdTextArea.attr("label", "I gave the answer above because...");
+		crowdTextArea.attr("rows", "5");
+		crowdTextArea.attr("max-rows", "5");
+		questionContainer.appendChild(crowdTextArea);
+		return questionContainer;
+	}
+
+	public static Element getConfidenceCrowdQuestionContainer() {
+		Element questionContainer = getBlankQuestionContainer("How confident are you in your answer?");
+		String[] optionNames = { "confidence-high", "confidence-medium", "confidence-low" };
+		String[] optionLabels = { "Highly Confident", "Somewhat Confident", "Not Confident" };
+		Element options = createCrowdRadioGroup(optionNames, optionLabels);
+		questionContainer.appendChild(options);
+		return questionContainer;
+	}
+
+	private static Element getBlankQuestionContainer(String question) {
+		Element container = new Element("div");
+		container.addClass("w3-container");
+		container.addClass("w3-margin");
+
+		Element questionHeader = new Element("h5");
+		questionHeader.text(question);
+		container.appendChild(questionHeader);
+		return container;
+	}
+
+	private static Element createCrowdRadioGroup(String[] optionNames, String[] optionLabels) {
+		Element crowdRadioGroup = new Element("crowd-radio-group");
+		crowdRadioGroup.attr("allow-empty-selection", false);
+		crowdRadioGroup.attr("disabled", false);
+
+		for (int i = 0; i < optionNames.length; i++) {
+			String name = optionNames[i];
+			String label = optionLabels[i];
+			Element crowdRadioButton = new Element("crowd-radio-button");
+			crowdRadioButton.attr("name", name);
+			crowdRadioButton.text(label);
+			crowdRadioGroup.appendChild(crowdRadioButton);
+		}
+		return crowdRadioGroup;
+	}
+}

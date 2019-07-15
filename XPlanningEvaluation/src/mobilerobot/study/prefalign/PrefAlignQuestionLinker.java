a7930c80bbfd22e7a97c6040a493fc58b00d772d
Roykrong Sukkerd <rsukkerd@andrew.cmu.edu>
2019-07-15 16:51:38 -0400

    getAllLinkedPrefAlignQuestionFiles()

--- XPlanningEvaluation/src/mobilerobot/study/prefalign/PrefAlignQuestionLinker.java
+++ XPlanningEvaluation/src/mobilerobot/study/prefalign/PrefAlignQuestionLinker.java
@@ -15,11 +15,18 @@ import mobilerobot.study.utilities.QuestionUtils;
 
 public class PrefAlignQuestionLinker {
 
+	private static final String QUESTION_NO_EXPL_FORMAT = "%s-agent%d";
+	private static final String QUESTION_EXPL_FORMAT = "%s-agent%d-explanation";
+
 	private Map<JSONObject, Set<File>> mQuestionDirsGroupedByCostStruct = new HashMap<>();
 	private File mRootDir;
+	private double mAlignProb;
+	private double mUnalignThreshold;
 
-	public PrefAlignQuestionLinker(File rootDir) {
+	public PrefAlignQuestionLinker(File rootDir, double alignProb, double unalignThreshold) {
 		mRootDir = rootDir;
+		mAlignProb = alignProb;
+		mUnalignThreshold = unalignThreshold;
 	}
 
 	public void groupQuestionDirsByCostStruct() throws IOException, ParseException {
@@ -34,7 +41,34 @@ public class PrefAlignQuestionLinker {
 		}
 	}
 
-	public File[][] getLinkedQuestionDirs(int numQuestions) {
+	public File[][] getAllLinkedPrefAlignQuestionFiles(int numQuestions, boolean withExplanation)
+			throws IOException, ParseException {
+		File[][] allLinkedPrefAlignQuestionFiles = new File[mQuestionDirsGroupedByCostStruct.size()][numQuestions];
+
+		File[][] allLinkedQuestionDirs = getAllLinkedQuestionDirs(numQuestions);
+		for (int i = 0; i <= allLinkedQuestionDirs.length; i++) {
+			// Each list of questions is assigned to a single participant
+			File[] linkedQuestionDirs = allLinkedQuestionDirs[i];
+
+			for (int j = 0; j < numQuestions; j++) {
+				File questionDir = linkedQuestionDirs[j];
+
+				// For each question dir, randomly select an agent
+				AgentRandomizer agentRand = new AgentRandomizer(questionDir, mAlignProb, mUnalignThreshold);
+				int agentIndex = agentRand.randomAgentIndex();
+
+				// question-mission[X]-agent[Y]
+				String questionRef = String.format(withExplanation ? QUESTION_EXPL_FORMAT : QUESTION_NO_EXPL_FORMAT,
+						questionDir.getName(), agentIndex);
+				File prefAlignQuestionFile = new File(questionDir, questionRef + ".html");
+				allLinkedPrefAlignQuestionFiles[i][j] = prefAlignQuestionFile;
+			}
+		}
+
+		return allLinkedPrefAlignQuestionFiles;
+	}
+
+	private File[][] getAllLinkedQuestionDirs(int numQuestions) {
 		File[][] allLinkedQuestionDirs = new File[mQuestionDirsGroupedByCostStruct.size()][numQuestions];
 		int i = 0;
 		for (Set<File> questionDirGroup : mQuestionDirsGroupedByCostStruct.values()) {

ad5812cb91f49968d92b0a0441318285862b0ffb
Roykrong Sukkerd <rsukkerd@andrew.cmu.edu>
2019-07-16 15:25:46 -0400

    Refactored PrefAlignQuestionHTMLLinker: createLinkedPrefAlignQuestionsDocuments()

--- XPlanningEvaluation/src/mobilerobot/study/prefalign/PrefAlignQuestionHTMLLinker.java
+++ XPlanningEvaluation/src/mobilerobot/study/prefalign/PrefAlignQuestionHTMLLinker.java
@@ -2,60 +2,100 @@ package mobilerobot.study.prefalign;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.json.simple.parser.ParseException;
-import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 
+import mobilerobot.study.utilities.HTMLGeneratorUtils;
 import mobilerobot.study.utilities.MTurkHTMLQuestionUtils;
+import mobilerobot.study.utilities.QuestionUtils;
 
 public class PrefAlignQuestionHTMLLinker {
 
 	private static final String[] DATA_TYPES = new String[] { "answer", "justification", "confidence" };
-
-	private PrefAlignQuestionLinker mQuestionLinker;
-
-	public PrefAlignQuestionHTMLLinker(File questionsRootDir, double alignProb, double unalignThreshold)
-			throws IOException, ParseException {
-		mQuestionLinker = new PrefAlignQuestionLinker(questionsRootDir, alignProb, unalignThreshold);
-		mQuestionLinker.groupQuestionDirsByCostStruct();
+	private static final Map<String, String[]> DATA_TYPE_OPTIONS = new HashMap<>();
+	static {
+		DATA_TYPE_OPTIONS.put("answer", new String[] { "yes", "no" });
+		DATA_TYPE_OPTIONS.put("confidence", new String[] { "high", "medium", "low" });
 	}
 
-	public void createLinkedPrefAlignQuestions(int numQuestions, boolean withExplanation)
-			throws IOException, ParseException {
-		File[][] allLinkedQuestionFiles = mQuestionLinker.getAllLinkedPrefAlignQuestionFiles(numQuestions,
-				withExplanation);
+	private PrefAlignQuestionLinker mQuestionLinker;
+	private PrefAlignQuestionHTMLGenerator mQuestionHTMLGenerator;
 
-		for (int i = 0; i < allLinkedQuestionFiles.length; i++) {
-			File[] linkedQuestionHTMLFiles = allLinkedQuestionFiles[i];
+	public PrefAlignQuestionHTMLLinker(File questionsRootDir, double alignProb, double unalignThreshold,
+			PrefAlignQuestionHTMLGenerator questionHTMLGenerator) throws IOException, ParseException {
+		mQuestionLinker = new PrefAlignQuestionLinker(questionsRootDir, alignProb, unalignThreshold);
+		mQuestionLinker.groupQuestionDirsByCostStruct();
+		mQuestionHTMLGenerator = questionHTMLGenerator;
+	}
+
+	public void createLinkedPrefAlignQuestions(int numQuestions, boolean withExplanation, File outDir)
+			throws IOException, ParseException {
+		File[][] allLinkedQuestionDirs = mQuestionLinker.getAllLinkedQuestionDirs(numQuestions);
+		int[][] allLinkedQuestionAgentIndices = mQuestionLinker.getAllLinkedQuestionAgentIndices(allLinkedQuestionDirs);
+
+		for (int i = 0; i < allLinkedQuestionDirs.length; i++) {
+			File[] linkedQuestionDirs = allLinkedQuestionDirs[i];
+			int[] linkedQuestionAgentIndices = allLinkedQuestionAgentIndices[i];
+
+			Document[] questionDocs = createLinkedPrefAlignQuestionsDocuments(linkedQuestionDirs,
+					linkedQuestionAgentIndices, withExplanation, outDir);
 		}
 	}
 
-	private Document createLinkedPrefAlignQuestionsDocument(File[] linkedQuestionHTMLFiles) throws IOException {
-		int numQuestions = linkedQuestionHTMLFiles.length;
+	private Document[] createLinkedPrefAlignQuestionsDocuments(File[] linkedQuestionDirs,
+			int[] linkedQuestionAgentIndices, boolean withExplanation, File outDir) throws IOException, ParseException {
+		int numQuestions = linkedQuestionDirs.length;
+		Document[] questionDocs = new Document[numQuestions];
+
 		for (int j = 0; j < numQuestions; j++) {
-			File questionHTMLFile = linkedQuestionHTMLFiles[j];
-			Document doc = Jsoup.parse(questionHTMLFile, "UTF-8", "../questions/");
+			File questionDir = linkedQuestionDirs[j];
+			int agentIndex = linkedQuestionAgentIndices[j];
+
+			Document questionDoc = mQuestionHTMLGenerator.createPrefAlignQuestionDocument(questionDir, agentIndex,
+					withExplanation, outDir);
 
 			// MTurk Crowd HTML
 			Element crowdScript = MTurkHTMLQuestionUtils.getCrowdHTMLScript();
 			Element mTurkCrowdFormDiv;
+			Element crowdFormActionScript;
 
 			if (j < numQuestions - 1) {
 				// Intermediate crowd-form
-				String nextUrl = ""; // TODO
+				File nextQuestionDir = linkedQuestionDirs[j + 1];
+				int nextAgentIndex = linkedQuestionAgentIndices[j + 1];
+				String nextQuestionDocName = QuestionUtils.getPrefAlignQuestionDocumentName(nextQuestionDir,
+						nextAgentIndex, withExplanation);
+
+				String nextUrl = nextQuestionDocName + ".html";
 				mTurkCrowdFormDiv = MTurkHTMLQuestionUtils.createIntermediateCrowdFormContainer(nextUrl);
+
+				crowdFormActionScript = MTurkHTMLQuestionUtils.getIntermediateCrowdFormNextOnClickScript(j, DATA_TYPES,
+						DATA_TYPE_OPTIONS);
 			} else {
 				// Last, submittable crowd-form
 				mTurkCrowdFormDiv = MTurkHTMLQuestionUtils.createSubmittableCrowdFormContainer(numQuestions,
 						DATA_TYPES);
+
+				crowdFormActionScript = MTurkHTMLQuestionUtils.getSubmittableCrowdFormOnSubmitScript(j, numQuestions,
+						DATA_TYPES, DATA_TYPE_OPTIONS);
 			}
 
-			doc.body().appendChild(crowdScript);
-			doc.body().appendChild(mTurkCrowdFormDiv);
+			questionDoc.body().appendChild(crowdScript);
+			questionDoc.body().appendChild(mTurkCrowdFormDiv);
+			questionDoc.body().appendChild(crowdFormActionScript);
+
+			// Write question HTML document to file
+			String questionDocName = QuestionUtils.getPrefAlignQuestionDocumentName(questionDir, agentIndex,
+					withExplanation);
+			HTMLGeneratorUtils.writeHTMLDocumentToFile(questionDoc, questionDocName, outDir);
+
+			questionDocs[j] = questionDoc;
 		}
 
-		return null;
+		return questionDocs;
 	}
 }

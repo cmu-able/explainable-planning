84c638d5d4bde1e4f0c8c55cf390822ef05719a8
Roykrong Sukkerd <rsukkerd@andrew.cmu.edu>
2019-07-16 15:24:21 -0400

    Refactored PrefAlignQuestionLinker: getAllLinkedQuestionDirs() and getAllLinkedQuestionAgentIndices()

--- XPlanningEvaluation/src/mobilerobot/study/prefalign/PrefAlignQuestionLinker.java
+++ XPlanningEvaluation/src/mobilerobot/study/prefalign/PrefAlignQuestionLinker.java
@@ -15,9 +15,6 @@ import mobilerobot.study.utilities.QuestionUtils;
 
 public class PrefAlignQuestionLinker {
 
-	private static final String QUESTION_NO_EXPL_FORMAT = "%s-agent%d";
-	private static final String QUESTION_EXPL_FORMAT = "%s-agent%d-explanation";
-
 	private Map<JSONObject, Set<File>> mQuestionDirsGroupedByCostStruct = new HashMap<>();
 	private File mRootDir;
 	private double mAlignProb;
@@ -41,34 +38,7 @@ public class PrefAlignQuestionLinker {
 		}
 	}
 
-	public File[][] getAllLinkedPrefAlignQuestionFiles(int numQuestions, boolean withExplanation)
-			throws IOException, ParseException {
-		File[][] allLinkedPrefAlignQuestionFiles = new File[mQuestionDirsGroupedByCostStruct.size()][numQuestions];
-
-		File[][] allLinkedQuestionDirs = getAllLinkedQuestionDirs(numQuestions);
-		for (int i = 0; i <= allLinkedQuestionDirs.length; i++) {
-			// Each list of questions is assigned to a single participant
-			File[] linkedQuestionDirs = allLinkedQuestionDirs[i];
-
-			for (int j = 0; j < numQuestions; j++) {
-				File questionDir = linkedQuestionDirs[j];
-
-				// For each question dir, randomly select an agent
-				AgentRandomizer agentRand = new AgentRandomizer(questionDir, mAlignProb, mUnalignThreshold);
-				int agentIndex = agentRand.randomAgentIndex();
-
-				// question-mission[X]-agent[Y]
-				String questionRef = String.format(withExplanation ? QUESTION_EXPL_FORMAT : QUESTION_NO_EXPL_FORMAT,
-						questionDir.getName(), agentIndex);
-				File prefAlignQuestionFile = new File(questionDir, questionRef + ".html");
-				allLinkedPrefAlignQuestionFiles[i][j] = prefAlignQuestionFile;
-			}
-		}
-
-		return allLinkedPrefAlignQuestionFiles;
-	}
-
-	private File[][] getAllLinkedQuestionDirs(int numQuestions) {
+	public File[][] getAllLinkedQuestionDirs(int numQuestions) {
 		File[][] allLinkedQuestionDirs = new File[mQuestionDirsGroupedByCostStruct.size()][numQuestions];
 		int i = 0;
 		for (Set<File> questionDirGroup : mQuestionDirsGroupedByCostStruct.values()) {
@@ -83,5 +53,27 @@ public class PrefAlignQuestionLinker {
 			i++;
 		}
 		return allLinkedQuestionDirs;
+	}
+
+	public int[][] getAllLinkedQuestionAgentIndices(File[][] allLinkedQuestionDirs) throws IOException, ParseException {
+		int numQuestions = allLinkedQuestionDirs[0].length;
+		int[][] allLinkedQuestionAgentIndices = new int[mQuestionDirsGroupedByCostStruct.size()][numQuestions];
+
+		for (int i = 0; i <= allLinkedQuestionDirs.length; i++) {
+			// Each list of questions is assigned to a single participant
+			File[] linkedQuestionDirs = allLinkedQuestionDirs[i];
+
+			for (int j = 0; j < numQuestions; j++) {
+				File questionDir = linkedQuestionDirs[j];
+
+				// For each question dir, randomly select an agent
+				AgentRandomizer agentRand = new AgentRandomizer(questionDir, mAlignProb, mUnalignThreshold);
+				int agentIndex = agentRand.randomAgentIndex();
+
+				allLinkedQuestionAgentIndices[i][j] = agentIndex;
+			}
+		}
+
+		return allLinkedQuestionAgentIndices;
 	}
 }

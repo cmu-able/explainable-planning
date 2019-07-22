f26c5b106c735c563ee944f772cfb31192c99484
Roykrong Sukkerd <rsukkerd@andrew.cmu.edu>
2019-07-22 13:38:28 -0400

    LinkedPrefAlignQuestions

---
+++ XPlanningEvaluation/src/mobilerobot/study/prefalign/LinkedPrefAlignQuestions.java
@@ -0,0 +1,99 @@
+package mobilerobot.study.prefalign;
+
+import java.io.File;
+import java.util.Arrays;
+
+import mobilerobot.study.utilities.QuestionUtils;
+
+public class LinkedPrefAlignQuestions {
+
+	/*
+	 * Cached hashCode -- Effective Java
+	 */
+	private volatile int hashCode;
+
+	private final File[] mLinkedQuestionDirs;
+	private final int[] mLinkedQuestionAgentIndices;
+
+	// Derived fields
+	private final String[] mLinkedQuestionDocNames;
+	private final String[] mLinkedQuestionDocNamesWithExplanation;
+
+	public LinkedPrefAlignQuestions(File[] linkedQuestionDirs, int[] linkedQuestionAgentIndices) {
+		mLinkedQuestionDirs = linkedQuestionDirs;
+		mLinkedQuestionAgentIndices = linkedQuestionAgentIndices;
+
+		mLinkedQuestionDocNames = createLinkedQuestionDocumentNames(linkedQuestionDirs, linkedQuestionAgentIndices,
+				false);
+		mLinkedQuestionDocNamesWithExplanation = createLinkedQuestionDocumentNames(linkedQuestionDirs,
+				linkedQuestionAgentIndices, true);
+	}
+
+	private String[] createLinkedQuestionDocumentNames(File[] linkedQuestionDirs, int[] linkedQuestionAgentIndices,
+			boolean withExplanation) {
+		String[] linkedQuestionDocNames = new String[linkedQuestionDirs.length];
+		for (int i = 0; i < linkedQuestionDirs.length; i++) {
+			File questionDir = linkedQuestionDirs[i];
+			int agentIndex = linkedQuestionAgentIndices[i];
+			String questionDocName = QuestionUtils.getPrefAlignQuestionDocumentName(questionDir, agentIndex,
+					withExplanation);
+
+			linkedQuestionDocNames[i] = questionDocName;
+		}
+		return linkedQuestionDocNames;
+	}
+
+	public int getNumQuestions() {
+		return mLinkedQuestionDirs.length;
+	}
+
+	public File[] getLinkedQuestionDirs() {
+		return mLinkedQuestionDirs;
+	}
+
+	public int[] getLinkedQuestionAgentIndices() {
+		return mLinkedQuestionAgentIndices;
+	}
+
+	public String[] getLinkedQuestionDocumentNames(boolean withExplanation) {
+		return withExplanation ? mLinkedQuestionDocNamesWithExplanation : mLinkedQuestionDocNames;
+	}
+
+	public File getQuestionDir(int questionIndex) {
+		return mLinkedQuestionDirs[questionIndex];
+	}
+
+	public int getQuestionAgentIndex(int questionIndex) {
+		return mLinkedQuestionAgentIndices[questionIndex];
+	}
+
+	public String getQuestionDocumentName(int questionIndex, boolean withExplanation) {
+		return withExplanation ? mLinkedQuestionDocNamesWithExplanation[questionIndex]
+				: mLinkedQuestionDocNames[questionIndex];
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (obj == this) {
+			return true;
+		}
+		if (!(obj instanceof LinkedPrefAlignQuestions)) {
+			return false;
+		}
+		LinkedPrefAlignQuestions linkedQuestions = (LinkedPrefAlignQuestions) obj;
+		return Arrays.equals(linkedQuestions.mLinkedQuestionDirs, mLinkedQuestionDirs)
+				&& Arrays.equals(linkedQuestions.mLinkedQuestionAgentIndices, mLinkedQuestionAgentIndices);
+	}
+
+	@Override
+	public int hashCode() {
+		int result = hashCode;
+		if (result == 0) {
+			result = 17;
+			result = 31 * result + Arrays.hashCode(mLinkedQuestionDirs);
+			result = 31 * result + Arrays.hashCode(mLinkedQuestionAgentIndices);
+			hashCode = result;
+		}
+		return hashCode;
+	}
+}

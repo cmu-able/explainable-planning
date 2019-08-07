c7d5e330fc99c8588655c7129fabf35e29175f2d
Roykrong Sukkerd <rsukkerd@andrew.cmu.edu>
2019-08-07 13:33:16 -0400

    Refactoring: use PrefAlignAgentGenerator in PrefAlignQuestionGenerator

--- XPlanningEvaluation/src/mobilerobot/study/prefalign/PrefAlignQuestionGenerator.java
+++ XPlanningEvaluation/src/mobilerobot/study/prefalign/PrefAlignQuestionGenerator.java
@@ -3,25 +3,17 @@ package mobilerobot.study.prefalign;
 import java.io.File;
 import java.io.IOException;
 import java.net.URISyntaxException;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map.Entry;
 
-import org.apache.commons.io.FileUtils;
-import org.apache.commons.io.FilenameUtils;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.ParseException;
 
 import examples.common.DSMException;
-import examples.common.XPlannerOutDirectories;
-import examples.mobilerobot.demo.MobileRobotXPlanner;
 import explanation.analysis.PolicyInfo;
 import explanation.analysis.QuantitativePolicy;
-import explanation.verbalization.QADecimalFormatter;
-import explanation.verbalization.VerbalizerSettings;
 import gurobi.GRBException;
 import language.domain.metrics.IQFunction;
 import language.exceptions.XMDPException;
@@ -36,28 +28,16 @@ import prism.PrismException;
 import solver.prismconnector.PrismConnector;
 import solver.prismconnector.exceptions.PrismConnectorException;
 import solver.prismconnector.exceptions.ResultParsingException;
-import uiconnector.ExplanationWriter;
-import uiconnector.PolicyWriter;
 
 public class PrefAlignQuestionGenerator implements IQuestionGenerator {
 
 	private static final double EQUALITY_TOL = 1e-5;
 
 	private QuestionViz mQuestionViz = new QuestionViz();
-	private MobileRobotXPlanner mXPlanner;
-	private XPlannerOutDirectories mOutputDirs;
+	private PrefAlignAgentGenerator mAgentGenerator;
 
 	public PrefAlignQuestionGenerator(File mapsJsonDir) throws IOException {
-		XPlannerOutDirectories outputDirs = FileIOUtils.createXPlannerOutDirectories();
-
-		// Configure verbalizer settings
-		VerbalizerSettings verbalizerSettings = new VerbalizerSettings();
-		verbalizerSettings.setDescribeCosts(false);
-		verbalizerSettings.setQADecimalFormatter(MobileRobotXPlanner.getQADecimalFormatter());
-		MobileRobotXPlanner.setVerbalizerOrdering(verbalizerSettings);
-
-		mXPlanner = new MobileRobotXPlanner(mapsJsonDir, outputDirs, verbalizerSettings);
-		mOutputDirs = outputDirs;
+		mAgentGenerator = new PrefAlignAgentGenerator(mapsJsonDir);
 	}
 
 	@Override
@@ -88,7 +68,7 @@ public class PrefAlignQuestionGenerator implements IQuestionGenerator {
 			QuantitativePolicy solnQuantPolicy = solnPolicyInfo.getQuantitativePolicy();
 
 			// Agent-0 is the aligned agent
-			createAgentData(questionDir, solnQuantPolicy, 0, missionFile);
+			createAgentData(questionDir, solnQuantPolicy, missionFile, 0);
 			indexedAgentQuantPolicies.add(0, solnQuantPolicy);
 
 			// The rest of the agents in this question dir are unique and different from agent-0,
@@ -113,7 +93,7 @@ public class PrefAlignQuestionGenerator implements IQuestionGenerator {
 				File agentMissionFile = lowerConvexHull.getMissionFile(agentQuantPolicy);
 
 				// Create agentPolicy[i].json, agentPolicyValues[i].json, and explanation-agent[i]
-				createAgentData(questionDir, agentQuantPolicy, agentIndex, agentMissionFile);
+				createAgentData(questionDir, agentQuantPolicy, agentMissionFile, agentIndex);
 
 				indexedAgentQuantPolicies.add(agentIndex, agentQuantPolicy);
 				agentIndex++;
@@ -136,30 +116,11 @@ public class PrefAlignQuestionGenerator implements IQuestionGenerator {
 		return lowerConvexHull.getNextMissionIndex();
 	}
 
-	private void createAgentData(File questionDir, QuantitativePolicy agentQuantPolicy, int agentIndex,
-			File agentMissionFile)
+	private void createAgentData(File questionDir, QuantitativePolicy agentQuantPolicy, File agentMissionFile,
+			int agentIndex)
 			throws IOException, PrismException, XMDPException, PrismConnectorException, GRBException, DSMException {
-		// Write each agent's proposed solution policy as json file at /output/question-missionX/
-		JSONObject agentPolicyJsonObj = PolicyWriter.writePolicyJSONObject(agentQuantPolicy.getPolicy());
-		writeAgentJSONObjectToFile(agentPolicyJsonObj, "agentPolicy.json", agentIndex, questionDir);
-
-		QADecimalFormatter decimalFormatter = MobileRobotXPlanner.getQADecimalFormatter();
-
-		// Write the QA values of each agent policy as json file
-		JSONObject agentPolicyValuesJsonObj = ExplanationWriter.writeQAValuesToJSONObject(agentQuantPolicy,
-				decimalFormatter);
-		writeAgentJSONObjectToFile(agentPolicyValuesJsonObj, "agentPolicyValues.json", agentIndex, questionDir);
-
-		// Create explanation dir that contains agent's corresponding mission file, solution policy, alternative
-		// policies, and explanation at /output/question-missionX/explanation-agent[i]/
-		createExplanation(questionDir, agentIndex, agentMissionFile);
-	}
-
-	private void writeAgentJSONObjectToFile(JSONObject agentJsonObj, String filename, int agentIndex, File questionDir)
-			throws IOException {
-		String agentFilename = FileIOUtils.insertIndexToFilename(filename, agentIndex);
-		File agentFile = FileIOUtils.createOutFile(questionDir, agentFilename);
-		FileIOUtils.prettyPrintJSONObjectToFile(agentJsonObj, agentFile);
+		mAgentGenerator.writeAgentPolicyAndValues(questionDir, agentQuantPolicy, agentIndex);
+		mAgentGenerator.createAgentExplanationDir(questionDir, agentMissionFile, agentIndex);
 	}
 
 	private void createSimpleCostStructureFile(File questionDir, SimpleCostStructure simpleCostStruct)
@@ -179,42 +140,6 @@ public class PrefAlignQuestionGenerator implements IQuestionGenerator {
 
 		File simpleCostStructFile = FileIOUtils.createOutFile(questionDir, "simpleCostStructure.json");
 		FileIOUtils.prettyPrintJSONObjectToFile(simpleCostStructJsonObj, simpleCostStructFile);
-	}
-
-	private void createExplanation(File questionDir, int agentIndex, File agentMissionFile)
-			throws IOException, PrismException, XMDPException, PrismConnectorException, GRBException, DSMException {
-		// Create explanation sub-directory for each agent: /output/question-missionX/explanation-agent[i]/
-		String explanationDirname = "explanation-agent" + agentIndex;
-		File explanationDir = FileIOUtils.createOutSubDir(questionDir, explanationDirname);
-
-		// Copy missionY.json file (corresponding mission of agent[i]) from /output/missions-of-{mapName}/ to the
-		// explanation sub-dir
-		// Note: missionY name is unique across all maps
-		Files.copy(agentMissionFile.toPath(), explanationDir.toPath().resolve(agentMissionFile.getName()));
-
-		// Check if missionY of agent[i] has already been explained
-		// If so, no need to run XPlanning on missionY.json again
-		Path explanationsOutPath = mOutputDirs.getExplanationsOutputPath();
-		String agentMissionName = FilenameUtils.removeExtension(agentMissionFile.getName());
-		String agentExplanationFilename = agentMissionName + "_explanation.json";
-		Path agentExplanationPath = explanationsOutPath.resolve(agentExplanationFilename);
-
-		if (!agentExplanationPath.toFile().exists()) {
-			// missionY_explanation.json does not exist -- missionY has not been explained yet
-			// Run XPlanning on missionY.json to create explanation for agent[i]'s policy
-			mXPlanner.runXPlanning(agentMissionFile);
-		}
-
-		// Copy solution policy, alternative policies, and explanation from XPlanningOutDirectories to the explanation
-		// sub-dir
-
-		// Copy solnPolicy.json and all altPolicy[j].json from /output/policies/missionY/ to the explanation sub-dir
-		Path policiesOutPath = mOutputDirs.getPoliciesOutputPath();
-		Path agentMissionPoliciesOutPath = policiesOutPath.resolve(agentMissionName);
-		FileUtils.copyDirectory(agentMissionPoliciesOutPath.toFile(), explanationDir);
-
-		// Copy missionY_explanation.json from /output/explanations/ to the explanation sub-dir
-		Files.copy(agentExplanationPath, explanationDir.toPath().resolve(agentExplanationFilename));
 	}
 
 	private JSONObject createAnswerKey(File missionFile, List<QuantitativePolicy> indexedAgentQuantPolicies,

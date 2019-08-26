f00bac8711af52048ef1bc51b49eee04b43355d3
Roykrong Sukkerd <rsukkerd@andrew.cmu.edu>
2019-08-26 13:38:58 -0400

    HITAssignmentsCollector: auto-reject insufficient-time answers for only non-validation questions

--- XPlanningEvaluation/src/mobilerobot/study/mturk/HITAssignmentsCollector.java
+++ XPlanningEvaluation/src/mobilerobot/study/mturk/HITAssignmentsCollector.java
@@ -1,7 +1,11 @@
 package mobilerobot.study.mturk;
 
 import java.io.IOException;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import javax.xml.parsers.ParserConfigurationException;
 
@@ -29,6 +33,9 @@ public class HITAssignmentsCollector {
 	private static final String REJECT_FEEDBACK = "Sorry, we could not approve your submission as you took too little time answering at least one of the questions.";
 	private static final String APPROVE_FEEDBACK = "Thank you for your participation.";
 
+	// Prefix pattern of each QuestionIdentifier
+	private static final Pattern QUESTION_ID_PREFIX_PATTERN = Pattern.compile("(question[0-9]+-).+");
+
 	private final MTurkClient mClient;
 
 	public HITAssignmentsCollector(MTurkClient client) {
@@ -50,22 +57,23 @@ public class HITAssignmentsCollector {
 		return listHITResponse.assignments();
 	}
 
-	public List<Assignment> collectPendingReviewHITAssignments(HITInfo hitInfo)
+	public List<Assignment> collectPendingReviewHITAssignments(HITInfo hitInfo, Set<String> validationQuestionDocNames)
 			throws ParserConfigurationException, SAXException, IOException {
 		// Collect the assignments that have been submitted for this HIT
 		// These assignments have not failed the Assignment Review Policy
 		List<Assignment> submittedAssignments = collectHITAssignments(hitInfo, AssignmentStatus.SUBMITTED);
-		autoRejectFailingAssignments(submittedAssignments);
+		autoRejectFailingAssignments(submittedAssignments, validationQuestionDocNames);
 
 		// Collect the remaining assignments that have not been auto-rejected
 		// These assignments will be reviewed manually to be approved or rejected
 		return collectHITAssignments(hitInfo, AssignmentStatus.SUBMITTED);
 	}
 
-	private void autoRejectFailingAssignments(List<Assignment> assignments)
+	public void autoRejectFailingAssignments(List<Assignment> assignments, Set<String> validationQuestionDocNames)
 			throws ParserConfigurationException, SAXException, IOException {
 		for (Assignment assignment : assignments) {
-			boolean autoReject = autoRejectAssignmentInsufficientTime(assignment);
+			// Only auto-reject insufficient-time answers of the non-validation questions
+			boolean autoReject = autoRejectAssignmentInsufficientTime(assignment, validationQuestionDocNames);
 
 			// Reject any assignment that took too little time
 			if (autoReject) {
@@ -77,11 +85,15 @@ public class HITAssignmentsCollector {
 		}
 	}
 
-	private boolean autoRejectAssignmentInsufficientTime(Assignment assignment)
+	private boolean autoRejectAssignmentInsufficientTime(Assignment assignment, Set<String> validationQuestionDocNames)
 			throws ParserConfigurationException, SAXException, IOException {
 		String answerXMLStr = assignment.answer();
 		Document answerXML = FileIOUtils.parseXMLString(answerXMLStr);
 		NodeList answerNodeList = answerXML.getElementsByTagName("Answer");
+
+		// Get prefix(es) of validation question(s): question[i]-
+		Set<String> validationQuestionPrefixes = getValidationQuestionPrefixes(answerNodeList,
+				validationQuestionDocNames);
 
 		for (int i = 0; i < answerNodeList.getLength(); i++) {
 			Node answerNode = answerNodeList.item(i);
@@ -89,8 +101,11 @@ public class HITAssignmentsCollector {
 			if (answerNode.getNodeType() == Node.ELEMENT_NODE) {
 				Element answerElement = (Element) answerNode;
 				String questionID = answerElement.getElementsByTagName("QuestionIdentifier").item(0).getTextContent();
+				String questionIDPrefix = getQuestionIDPrefix(questionID);
 
-				if (questionID.matches("question[0-9]+-elapsedTime")) {
+				// Only consider elapsed time of non-validation questions
+				if (questionID.matches("question[0-9]+-elapsedTime")
+						&& !validationQuestionPrefixes.contains(questionIDPrefix)) {
 					String elapsedTimeStr = answerElement.getElementsByTagName("FreeText").item(0).getTextContent();
 					long elapsedTimeinMS = Long.parseLong(elapsedTimeStr);
 
@@ -101,6 +116,40 @@ public class HITAssignmentsCollector {
 			}
 		}
 		return false;
+	}
+
+	private Set<String> getValidationQuestionPrefixes(NodeList answerNodeList, Set<String> validationQuestionDocNames) {
+		Set<String> validationQuestionPrefixes = new HashSet<>();
+
+		for (int i = 0; i < answerNodeList.getLength(); i++) {
+			Node answerNode = answerNodeList.item(i);
+
+			if (answerNode.getNodeType() == Node.ELEMENT_NODE) {
+				Element answerElement = (Element) answerNode;
+				String questionID = answerElement.getElementsByTagName("QuestionIdentifier").item(0).getTextContent();
+
+				if (questionID.matches("question[0-9]+-ref")) {
+					// question[i]-ref maps to the document name of that question, e.g., question-mission[n]-agent[m]-explanation
+					String questionRef = answerElement.getElementsByTagName("FreeText").item(0).getTextContent();
+
+					// If this question is a validation question, get its prefix: question[i]-
+					if (validationQuestionDocNames.contains(questionRef)) {
+						String questionIDPrefix = getQuestionIDPrefix(questionID);
+						validationQuestionPrefixes.add(questionIDPrefix);
+					}
+				}
+			}
+		}
+		return validationQuestionPrefixes;
+	}
+
+	private String getQuestionIDPrefix(String questionID) {
+		Matcher m = QUESTION_ID_PREFIX_PATTERN.matcher(questionID);
+		if (m.find()) {
+			return m.group(1);
+		}
+		throw new IllegalArgumentException(
+				"The QuestionIdentifier \"" + questionID + "\" does not have the correct prefix.");
 	}
 
 	public List<Assignment> collectApprovedHITAssignments(HITInfo hitInfo) {

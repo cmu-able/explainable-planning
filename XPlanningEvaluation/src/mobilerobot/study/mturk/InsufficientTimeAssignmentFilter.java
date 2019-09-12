0cdaef998595c44bb49f12ee6aa53a17ee3d94a4
Roykrong Sukkerd <rsukkerd@andrew.cmu.edu>
2019-09-12 16:22:38 -0400

    Minor change in InsufficientTimeAssignmentFilter

--- XPlanningEvaluation/src/mobilerobot/study/mturk/InsufficientTimeAssignmentFilter.java
+++ XPlanningEvaluation/src/mobilerobot/study/mturk/InsufficientTimeAssignmentFilter.java
@@ -19,8 +19,8 @@ public class InsufficientTimeAssignmentFilter implements IAssignmentFilter {
 	private static final long MIN_ELAPSED_TIME_MS = 30 * 1000L; // minimum of 30 seconds/question
 	private static final String REJECT_FEEDBACK = "Sorry, we could not approve your submission as you took too little time answering at least one of the questions.";
 
-	// Prefix pattern of each QuestionIdentifier
-	private static final Pattern QUESTION_ID_PREFIX_PATTERN = Pattern.compile("(question[0-9]+-).+");
+	// Prefix pattern of each question key
+	private static final Pattern QUESTION_KEY_PREFIX_PATTERN = Pattern.compile("(question[0-9]+-).+");
 
 	private final Set<String> mValidationQuestionDocNames;
 
@@ -51,6 +51,11 @@ public class InsufficientTimeAssignmentFilter implements IAssignmentFilter {
 		return true;
 	}
 
+	@Override
+	public String getRejectFeedback() {
+		return REJECT_FEEDBACK;
+	}
+
 	private Set<String> getValidationQuestionPrefixes(Assignment assignment)
 			throws ParserConfigurationException, SAXException, IOException, ParseException {
 		JSONObject answerJsonObj = AssignmentsCollector.getAssignmentAnswerJSONObjectFromFreeText(assignment);
@@ -75,17 +80,12 @@ public class InsufficientTimeAssignmentFilter implements IAssignmentFilter {
 	}
 
 	private String getQuestionIDPrefix(String questionID) {
-		Matcher m = QUESTION_ID_PREFIX_PATTERN.matcher(questionID);
+		Matcher m = QUESTION_KEY_PREFIX_PATTERN.matcher(questionID);
 		if (m.find()) {
 			return m.group(1);
 		}
 		throw new IllegalArgumentException(
 				"The QuestionIdentifier \"" + questionID + "\" does not have the correct prefix.");
-	}
-
-	@Override
-	public String getRejectFeedback() {
-		return REJECT_FEEDBACK;
 	}
 
 }

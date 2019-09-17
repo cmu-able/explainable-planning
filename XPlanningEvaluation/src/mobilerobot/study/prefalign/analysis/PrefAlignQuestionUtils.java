f868e540bb8ef5f4da714cd40afc35a16a91c3a1
Roykrong Sukkerd <rsukkerd@andrew.cmu.edu>
2019-09-17 17:42:13 -0400

    PrefAlignQuestionUtils: added getAgentPolicyCost()

--- XPlanningEvaluation/src/mobilerobot/study/prefalign/analysis/PrefAlignQuestionUtils.java
+++ XPlanningEvaluation/src/mobilerobot/study/prefalign/analysis/PrefAlignQuestionUtils.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.ParseException;
 
+import mobilerobot.study.utilities.QuestionUtils;
 import mobilerobot.utilities.FileIOUtils;
 import uiconnector.JSONSimpleParserUtils;
 
@@ -19,11 +20,51 @@ public class PrefAlignQuestionUtils {
 	private static final String JSON_EXTENSION = ".json";
 
 	public static double getAgentAlignmentScore(File questionDir, int agentIndex) throws IOException, ParseException {
-		// There is only 1 scoreCard.json per question dir
-		File scoreCardJsonFile = FileIOUtils.listFilesWithContainFilter(questionDir, "scoreCard", JSON_EXTENSION)[0];
-		JSONObject scoreCardJsonObj = FileIOUtils.readJSONObjectFromFile(scoreCardJsonFile);
+		JSONObject scoreCardJsonObj = QuestionUtils.getScoreCardJSONObject(questionDir);
 		String agentKey = String.format(AGENT_KEY_FORMAT, agentIndex);
 		return JSONSimpleParserUtils.parseDouble(scoreCardJsonObj.get(agentKey));
+	}
+
+	public static double getAgentPolicyCost(File questionDir, int agentIndex) throws IOException, ParseException {
+		// simpleCostStructure.json
+		JSONObject costStructJsonObj = QuestionUtils.getSimpleCostStructureJSONObject(questionDir);
+
+		// agentPolicyValues[i].json
+		String agentPolicyValuesName = String.format("agentPolicyValues%d", agentIndex);
+		File agentPolicyValuesJsonFile = FileIOUtils.listFilesWithContainFilter(questionDir, agentPolicyValuesName,
+				JSON_EXTENSION)[0];
+		JSONObject agentPolicyValuesJsonObj = FileIOUtils.readJSONObjectFromFile(agentPolicyValuesJsonFile);
+
+		// Total cost of agent's policy
+		double totalCost = 0;
+		for (Object key : costStructJsonObj.keySet()) {
+			String qaName = (String) key;
+
+			// cost per unit
+			JSONObject qaUnitCostJsonObj = (JSONObject) costStructJsonObj.get(qaName);
+			String descriptiveUnit = (String) qaUnitCostJsonObj.get("unit");
+			String unitCostStr = (String) qaUnitCostJsonObj.get("cost");
+			double unit = Double.parseDouble(descriptiveUnit.split(" ")[0]);
+			int unitCost = Integer.parseInt(unitCostStr); // every unit cost is an integer 
+
+			// QA value of policy
+			Object qaValueObj = agentPolicyValuesJsonObj.get(qaName);
+			String qaValueStr;
+			if (qaValueObj instanceof JSONObject) {
+				JSONObject qaValueJsonObj = (JSONObject) qaValueObj;
+				qaValueStr = (String) qaValueJsonObj.get("Value");
+			} else {
+				qaValueStr = (String) qaValueObj;
+			}
+			double qaValue = Double.parseDouble(qaValueStr);
+
+			// QA cost of policy
+			double qaCost = qaValue / unit * unitCost;
+
+			totalCost += qaCost;
+		}
+
+		return totalCost;
 	}
 
 	public static String getAgentAlignmentAnswer(File questionDir, int agentIndex) throws IOException, ParseException {

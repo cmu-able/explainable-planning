d3df9553bbff22f59f30c86efb8be4269aaf9f49
Roykrong Sukkerd <rsukkerd@andrew.cmu.edu>
2019-10-22 12:06:32 -0400

    Keep only 1 statistical_analysis.R and updated results.txt

--- analysis/statistical_analysis.R
+++ analysis/statistical_analysis.R
@@ -1,7 +1,54 @@
-data <- read.csv("~/Projects/explainable-planning/analysis/data_3qs.csv")
 library(lme4)
-data.model <- lmer(accuracy ~ group + (1|participant) + (1|question.ref), data = data, REML = FALSE)
-print(summary(data.model))
-data.null <- lmer(accuracy ~ 1 + (1|participant) + (1|question.ref), data = data, REML = FALSE)
-data.model <- lmer(accuracy ~ group + (1|participant) + (1|question.ref), data = data, REML = FALSE)
-anova(data.null, data.model)
+library(lmerTest)
+# library(r2glmm) # R squared
+library(MuMIn) # R squared
+
+# setwd("~/Downloads")
+data = read.csv("~/Projects/explainable-planning/analysis/data_3qs.csv")
+names(data)
+
+str(data)
+data$accuracy = as.factor(data$accuracy)
+table(data$accuracy)
+
+## WOW, look at this!
+unique(data$group)
+boxplot(list(control = data[data$group=="control",]$score, 
+             treatment = data[data$group=="experimental",]$score))
+
+boxplot(list(control = as.numeric(data[data$group=="control",]$accuracy), 
+             treatment = as.numeric(data[data$group=="experimental",]$accuracy)))
+
+
+m1 = glmer(accuracy ~ 
+            group 
+            + (1|participant) 
+            + (1|question.ref)
+          , family = "binomial"
+          , data = data)
+
+summary(m1) # experimental group are exp(1.3352) = 3.8 times more likely to answer correctly!
+
+# R2m: describes the proportion of variance explained by the fixed factor(s) alone
+# R2c: describes the proportion of variance explained by both the fixed and random factors
+r.squaredGLMM(m1) # goodness of fit of model, without (left) and with (right) random effects
+
+# Doesn't make sense to use ANOVA here since we only have 1 fixed effect
+# Instead, report marignal R^2 (R2m) and conditional R^2 (R2c)
+anova(m1) 
+
+table(data$score)
+
+m2 = lmer(score ~ 
+             group 
+           + (1|participant) 
+           + (1|question.ref)
+           , data = data)
+
+summary(m2)
+r.squaredGLMM(m2)
+
+# Doesn't make sense to use ANOVA here since we only have 1 fixed effect
+# Instead, report marignal R^2 (R2m) and conditional R^2 (R2c)
+anova(m2)
+

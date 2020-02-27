2e6389cf25f4b8be994088de69d3ccac971b4e86
Roykrong Sukkerd <rsukkerd@andrew.cmu.edu>
2020-02-27 16:43:04 -0500

    Added PolicyAnalyzer and QueryPolicyInfo to compute QA values of a policy starting from a query state

---
+++ XPlanningIterative/src/analysis/PolicyAnalyzer.java
@@ -0,0 +1,49 @@
+package analysis;
+
+import explanation.analysis.PolicyInfo;
+import language.exceptions.XMDPException;
+import language.mdp.StateVarTuple;
+import language.mdp.XMDP;
+import language.objectives.CostCriterion;
+import language.policy.Policy;
+import models.hmodel.QueryPolicyInfo;
+import prism.PrismException;
+import solver.prismconnector.PrismConnector;
+import solver.prismconnector.PrismConnectorSettings;
+import solver.prismconnector.exceptions.ResultParsingException;
+
+public class PolicyAnalyzer {
+
+	private XMDP mXMDP;
+	private CostCriterion mCostCriterion;
+	private PrismConnectorSettings mPrismConnSettings;
+
+	public PolicyAnalyzer(XMDP xmdp, CostCriterion costCriterion, PrismConnectorSettings prismConnSettings) {
+		mXMDP = xmdp;
+		mCostCriterion = costCriterion;
+		mPrismConnSettings = prismConnSettings;
+	}
+
+	public QueryPolicyInfo computeQueryPolicyInfo(Policy policy, StateVarTuple queryState)
+			throws PrismException, ResultParsingException, XMDPException {
+		// Create a new PrismConnector each time a new query state is given
+		// Use the query state as initial state to compute QA value
+
+		// Create XMDP model identical to the original model, but with the query state as initial state
+		XMDP queryXMDP = new XMDP(mXMDP.getStateSpace(), mXMDP.getActionSpace(), queryState, mXMDP.getGoal(),
+				mXMDP.getTransitionFunction(), mXMDP.getQSpace(), mXMDP.getCostFunction());
+
+		// Create Prism connector (without the query state as absorbing state)
+		PrismConnector prismConnector = new PrismConnector(queryXMDP, mCostCriterion, mPrismConnSettings);
+
+		// Compute QA values, objective cost, scaled QA costs of the policy, starting from the query state
+		PolicyInfo policyInfo = prismConnector.buildPolicyInfo(policy);
+
+		QueryPolicyInfo queryPolicyInfo = new QueryPolicyInfo(queryState, policyInfo);
+
+		// Close down PRISM
+		prismConnector.terminate();
+
+		return queryPolicyInfo;
+	}
+}

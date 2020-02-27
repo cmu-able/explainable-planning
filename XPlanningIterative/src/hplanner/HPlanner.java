3d1b897e9a4129a210332359ab5aa28733122097
Roykrong Sukkerd <rsukkerd@andrew.cmu.edu>
2020-02-26 22:17:46 -0500

    Initial HPlanner

---
+++ XPlanningIterative/src/hplanner/HPlanner.java
@@ -0,0 +1,15 @@
+package hplanner;
+
+import explanation.analysis.PolicyInfo;
+import solver.prismconnector.PrismConnector;
+
+public class HPlanner {
+
+	public HPlanner() {
+
+	}
+
+	public PolicyInfo computeHPolicy(PrismConnector prismConnectorForHModel, double queryQAValueConstraint) {
+		return null;
+	}
+}

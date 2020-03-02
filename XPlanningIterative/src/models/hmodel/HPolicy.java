d64acbad10acefe35699ec51748a3801899dd550
Roykrong Sukkerd <rsukkerd@andrew.cmu.edu>
2020-03-02 17:04:32 -0500

    HPolicy

---
+++ XPlanningIterative/src/models/hmodel/HPolicy.java
@@ -0,0 +1,81 @@
+package models.hmodel;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import explanation.analysis.PolicyInfo;
+import language.domain.models.IAction;
+import language.mdp.StateVarTuple;
+import language.policy.Decision;
+import language.policy.Policy;
+
+public class HPolicy {
+
+	/*
+	 * Cached hashCode -- Effective Java
+	 */
+	private volatile int hashCode;
+
+	private Map<StateVarTuple, PolicyInfo> mPartialHPolicies = new HashMap<>();
+
+	// Total hypothetical policy -- built from 1 or more partial HPolicies
+	private Policy mTotalHPolicy;
+
+	public HPolicy(PolicyInfo queryPolicyInfo, StateVarTuple queryState, IAction queryAction) {
+		// Initialize total HPolicy to be the query policy (this will be overridden later)
+		mTotalHPolicy = new Policy(queryPolicyInfo.getPolicy());
+
+		// In the total HPolicy, a_query is taken in s_query
+		mTotalHPolicy.put(queryState, queryAction);
+	}
+
+	public void mapNewInitialStateToPartialHPolicyInfo(StateVarTuple newIniState, PolicyInfo partialHPolicyInfo) {
+		mPartialHPolicies.put(newIniState, partialHPolicyInfo);
+
+		// Building up total hypothetical policy
+		for (Decision decision : partialHPolicyInfo.getPolicy()) {
+			StateVarTuple state = decision.getState();
+			IAction action = decision.getAction();
+
+			// Add partial HPolicy, starting at each new initial state, to the total HPolicy
+			// (override the query policy content)
+			mTotalHPolicy.put(state, action);
+		}
+	}
+
+	public Policy getTotalHPolicy() {
+		// Total HPolicy is the query policy overridden by:
+		// - (s_query, a_query) decision
+		// - Partial HPolicy starting at each new initial state (i.e., each resulting state of s_query, a_query)
+		return mTotalHPolicy;
+	}
+
+	public PolicyInfo getPartialHPolicyInfo(StateVarTuple newIniState) {
+		return mPartialHPolicies.get(newIniState);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (obj == this) {
+			return true;
+		}
+		if (!(obj instanceof HPolicy)) {
+			return false;
+		}
+		HPolicy hPolicy = (HPolicy) obj;
+		return hPolicy.mPartialHPolicies.equals(mPartialHPolicies) && hPolicy.mTotalHPolicy.equals(mTotalHPolicy);
+	}
+
+	@Override
+	public int hashCode() {
+		int result = hashCode;
+		if (result == 0) {
+			result = 17;
+			result = 31 * result + mPartialHPolicies.hashCode();
+			result = 31 * result + mTotalHPolicy.hashCode();
+			hashCode = result;
+		}
+		return hashCode;
+	}
+
+}

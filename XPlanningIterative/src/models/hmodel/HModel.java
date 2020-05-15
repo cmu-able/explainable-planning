46113c68a1ef20c532cdb1318b90a81d331d7587
Roykrong Sukkerd <rsukkerd@andrew.cmu.edu>
2020-05-15 15:40:23 -0400

    HModel: handle pre-configuration actions in query

--- XPlanningIterative/src/models/hmodel/HModel.java
+++ XPlanningIterative/src/models/hmodel/HModel.java
@@ -1,11 +1,14 @@
 package models.hmodel;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
+import java.util.stream.StreamSupport;
 
 import language.domain.metrics.IQFunction;
 import language.domain.models.ActionDefinition;
@@ -34,11 +37,28 @@ public class HModel<E extends IAction> {
 	// Original XMDP
 	private XMDP mOriginalXMDP;
 
-	// Why-not (s,a) query
+	// -- Why-not (s,a) query -- //
+	// Optionally with pre-configuration actions
+
+	// First query state (may not be the final query state)
 	private StateVarTuple mQueryState;
+
+	// Main query action (final query action)
 	private E mQueryAction;
 
+	// Configuration actions to apply prior to the main query action
+	private List<? extends IAction> mPreConfigActions;
+
+	// -- End why-not query -- //
+
 	// -- Derived fields -- //
+
+	// Mapping from each state to its pre-configuration action, starting from the query state
+	private Map<StateVarTuple, IAction> mPreConfigPolicyConstraints;
+
+	// Final query state after applying all pre-configuration actions
+	// The main query action will be applied to this final query state
+	private StateVarTuple mFinalQueryState;
 
 	// Resulting states and their probabilities
 	private ProbabilisticEffect mProbEffectOfQuery;
@@ -52,24 +72,75 @@ public class HModel<E extends IAction> {
 	private Map<StateVarTuple, Map<IQFunction<?, ?>, Double>> mQueryQAValueConstraints = new HashMap<>();
 
 	public HModel(XMDP originalXMDP, StateVarTuple queryState, E queryAction) throws XMDPException {
+		this(originalXMDP, queryState, queryAction, new ArrayList<>());
+	}
+
+	public HModel(XMDP originalXMDP, StateVarTuple queryState, E queryAction, List<? extends IAction> preConfigActions)
+			throws XMDPException {
 		mOriginalXMDP = originalXMDP;
 		mQueryState = queryState;
 		mQueryAction = queryAction;
-		mProbEffectOfQuery = computeProbabilisticEffect(queryState, queryAction);
+		mPreConfigActions = preConfigActions;
+
+		// Derived fields
+		mPreConfigPolicyConstraints = buildPreConfigPolicyConstraints();
+		mProbEffectOfQuery = computeProbabilisticEffect(mFinalQueryState, queryAction);
 		createQueryXMDPs();
 	}
 
-	private ProbabilisticEffect computeProbabilisticEffect(StateVarTuple queryState, E queryAction)
-			throws XMDPException {
-		ActionDefinition<E> actionDef = mOriginalXMDP.getActionSpace().getActionDefinition(queryAction);
+	/**
+	 * Build pre-configuration policy constraints, in a form of a mapping from each state to its pre-configuration
+	 * action, starting from the query state.
+	 * 
+	 * This mapping does NOT include the final query state and the final (main) query action.
+	 * 
+	 * @return Mapping from each state to its pre-configuration action, starting from the query state
+	 * @throws XMDPException
+	 */
+	private Map<StateVarTuple, IAction> buildPreConfigPolicyConstraints() throws XMDPException {
+		Map<StateVarTuple, IAction> policyConstraints = new HashMap<>();
 
-		// Use <? super E> because if the query action is a constituent action that doesn't have its own
+		// currState must contain all variables, because it will be placed in a Policy
+		StateVarTuple currState = mQueryState;
+
+		for (IAction preConfigAction : mPreConfigActions) {
+			// Map state -> pre-configuration action
+			policyConstraints.put(currState, preConfigAction);
+
+			ProbabilisticEffect probEffect = computeProbabilisticEffect(currState, preConfigAction);
+
+			// Assume that any pre-configuration action has deterministic effect
+
+			// detEffect may not contain all variables
+			Effect detEffect = StreamSupport.stream(probEffect.spliterator(), false).filter(e -> e.getValue() == 1)
+					.map(Map.Entry::getKey).findFirst().orElse(null);
+
+			// nextState must contain all variables
+			StateVarTuple nextState = new StateVarTuple();
+			nextState.addStateVarTuple(currState); // initialize vars of nextState with values of currState
+			nextState.addStateVarTuple(detEffect); // update (some) vars of nextState with values of detEffect
+
+			currState = nextState;
+		}
+
+		// Final state after applying all pre-configuration actions
+		// Final query state is NOT included in the pre-configuration policy constraints
+		mFinalQueryState = currState;
+
+		return policyConstraints;
+	}
+
+	private <T extends IAction> ProbabilisticEffect computeProbabilisticEffect(StateVarTuple state, T action)
+			throws XMDPException {
+		ActionDefinition<T> actionDef = mOriginalXMDP.getActionSpace().getActionDefinition(action);
+
+		// Use <? super T> because if the query action is a constituent action that doesn't have its own
 		// actionPSO, we need to use its parent composite actionPSO
-		FactoredPSO<? super E> actionPSO = null;
+		FactoredPSO<? super T> actionPSO = null;
 
 		// If the query action is a constituent action, we must use its parent composite actionPSO
 		// to get the full effect of the query action
-		FactoredPSO<? super E> parentCompositeActionPSO = null;
+		FactoredPSO<? super T> parentCompositeActionPSO = null;
 
 		// Check if the query action has its own actionPSO
 		if (mOriginalXMDP.getTransitionFunction().hasActionPSO(actionDef)) {
@@ -96,25 +167,25 @@ public class HModel<E extends IAction> {
 			// The query action has its own actionPSO
 			// Add the direct probabilistic effects of the query action
 			Set<EffectClass> directEffectClasses = actionPSO.getIndependentEffectClasses();
-			addProbabilisticEffects(directEffectClasses, actionPSO, queryState, queryAction, probEffects);
+			addProbabilisticEffects(directEffectClasses, actionPSO, state, action, probEffects);
 		}
 
 		if (parentCompositeActionPSO != null) {
 			// The query action has parent composite actionPSO
 			// Add the parent probabilistic effects of the query action
 			Set<EffectClass> parentEffectClasses = parentCompositeActionPSO.getIndependentEffectClasses();
-			addProbabilisticEffects(parentEffectClasses, parentCompositeActionPSO, queryState, queryAction,
-					probEffects);
+			addProbabilisticEffects(parentEffectClasses, parentCompositeActionPSO, state, action, probEffects);
 		}
 
 		// Combined probabilistic effect of the (s,q) query
 		return combineProbabilisticEffects(probEffects);
 	}
 
-	private void addProbabilisticEffects(Set<EffectClass> effectClasses, FactoredPSO<? super E> actionPSO,
-			StateVarTuple queryState, E queryAction, Set<ProbabilisticEffect> probEffectsOutput) throws XMDPException {
+	private <T extends IAction> void addProbabilisticEffects(Set<EffectClass> effectClasses,
+			FactoredPSO<? super T> actionPSO, StateVarTuple queryState, T queryAction,
+			Set<ProbabilisticEffect> probEffectsOutput) throws XMDPException {
 		for (EffectClass effectClass : effectClasses) {
-			IActionDescription<? super E> actionDesc = actionPSO.getActionDescription(effectClass);
+			IActionDescription<? super T> actionDesc = actionPSO.getActionDescription(effectClass);
 			DiscriminantClass discrClass = actionDesc.getDiscriminantClass();
 
 			Discriminant discriminant = new Discriminant(discrClass);
@@ -184,11 +255,11 @@ public class HModel<E extends IAction> {
 			// New initial state
 			StateVarTuple newIniState = new StateVarTuple();
 
-			// Add effect of the query (s_query, a_query)
+			// Add effect of the query (s_query_final, a_query_final)
 			newIniState.addStateVarTuple(effect);
 
 			// Set values of the variables unaffected by the query, in the new initial state
-			for (StateVar<IStateVarValue> stateVar : mQueryState) {
+			for (StateVar<IStateVarValue> stateVar : mFinalQueryState) {
 				if (!effect.contains(stateVar.getDefinition())) {
 					newIniState.addStateVar(stateVar);
 				}
@@ -208,14 +279,43 @@ public class HModel<E extends IAction> {
 		return mOriginalXMDP;
 	}
 
-	public StateVarTuple getQueryState() {
-		return mQueryState;
+	/**
+	 * Get pre-configuration policy constraints, in a form of a mapping from each state to its pre-configuration action,
+	 * starting from the query state.
+	 * 
+	 * This mapping does NOT include the final query state and the final (main) query action.
+	 * 
+	 * @return Mapping from each state to its pre-configuration action, starting from the query state
+	 * @throws XMDPException
+	 */
+	public Map<StateVarTuple, IAction> getPreConfigPolicyConstraints() {
+		return mPreConfigPolicyConstraints;
 	}
 
-	public E getQueryAction() {
+	/**
+	 * Get the final query state, which is the state after applying all pre-configuration actions.
+	 * 
+	 * @return Final query state
+	 */
+	public StateVarTuple getFinalQueryState() {
+		return mFinalQueryState;
+	}
+
+	/**
+	 * Get the final query action, which is the main query action after pre-configuration actions, if any. This main
+	 * query action can have probabilistic effects.
+	 * 
+	 * @return Final query action
+	 */
+	public E getFinalQueryAction() {
 		return mQueryAction;
 	}
 
+	/**
+	 * Get probabilistic effect of the full query, including all pre-configuration actions and the main query action.
+	 * 
+	 * @return Probabilistic effect of the full query
+	 */
 	public ProbabilisticEffect getProbabilisticEffectOfQuery() {
 		return mProbEffectOfQuery;
 	}
@@ -223,7 +323,7 @@ public class HModel<E extends IAction> {
 	/**
 	 * For simplicity, assume for now that the query cannot lead to more than 1 non-absorbing state.
 	 * 
-	 * @return Destination states of the query
+	 * @return Destination states of the full query
 	 */
 	public Set<StateVarTuple> getAllDestStatesOfQuery() {
 		return mQueryXMDPs.keySet();
@@ -256,7 +356,7 @@ public class HModel<E extends IAction> {
 		}
 		HModel<?> hModel = (HModel<?>) obj;
 		return hModel.mOriginalXMDP.equals(mOriginalXMDP) && hModel.mQueryState.equals(mQueryState)
-				&& hModel.mQueryAction.equals(mQueryAction)
+				&& hModel.mQueryAction.equals(mQueryAction) && hModel.mPreConfigActions.equals(mPreConfigActions)
 				&& hModel.mQueryQAValueConstraints.equals(mQueryQAValueConstraints);
 	}
 
@@ -268,6 +368,7 @@ public class HModel<E extends IAction> {
 			result = 31 * result + mOriginalXMDP.hashCode();
 			result = 31 * result + mQueryState.hashCode();
 			result = 31 * result + mQueryAction.hashCode();
+			result = 31 * result + mPreConfigActions.hashCode();
 			result = 31 * result + mQueryQAValueConstraints.hashCode();
 			hashCode = result;
 		}
